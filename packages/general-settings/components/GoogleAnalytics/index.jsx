99ad7a13681d11935b1924bfeb25033907cd4665
lolaodelola <damzcodes@gmail.com>
2018-10-31 11:21:33 +0000

    customisation form style

--- packages/general-settings/components/GoogleAnalytics/index.jsx
+++ packages/general-settings/components/GoogleAnalytics/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {
   Text, Toggle, Divider,
-  Button,
+  Button, Input,
 } from '@bufferapp/components';
 
 const enableGoogleAnalyticsStyle = {
@@ -12,7 +12,7 @@ const enableGoogleAnalyticsStyle = {
   marginBottom: '0.5rem',
 };
 
-const generalTextWrapperStyle = {
+const generalWrapperStyle = {
   display: 'flex',
   marginBottom: '0.5rem',
 };
@@ -29,12 +29,16 @@ const toggleTextWrapperStyle = {
   width: '80%',
 };
 
-const enableGoogleAnalyticsToggleStyle = {
+const switchStyle = {
   marginBottom: '1.5rem',
   marginTop: '1rem',
   textAlign: 'right',
   whiteSpace: 'nowrap',
 };
+
+const inputStyle = {
+  marginRight: '0.5rem',
+}
 
 
 const GoogleAnalytics = ({
@@ -51,7 +55,7 @@ const GoogleAnalytics = ({
         Google Analytics Campaign Tracking
       </Text>
     </div>
-    <div style={generalTextWrapperStyle}>
+    <div style={generalWrapperStyle}>
       <Text>
         With campaign tracking enabled in Buffer, you will be able to see how much traffic
         you receive from social media posts directly inside your Google Analytics dashboard. You
@@ -73,7 +77,7 @@ const GoogleAnalytics = ({
           (This will override any existing UTM parameters)
         </Text>
       </div>
-      <div style={enableGoogleAnalyticsToggleStyle}>
+      <div style={switchStyle}>
         <Toggle
           on={console.log("enabled")}
           onClick={console.log("clicked")}
@@ -81,6 +85,31 @@ const GoogleAnalytics = ({
         />
       </div>
     </div>
+    <form style={generalWrapperStyle}>
+      <div style={inputStyle}>
+        <Input
+          label={'Campaign Name'}
+          placeholder={'buffer'}
+        />
+      </div>
+      <div style={inputStyle}>
+        <Input
+          label={'Campaign Source'}
+          placeholder={'bufferapp.com'}
+        />
+      </div>
+      <div style={inputStyle}>
+        <Input
+          label={'Campaign Medium'}
+          placeholder={'social'}
+        />
+      </div>
+      <div style={switchStyle}>
+        <Button>
+          Update Tracking
+        </Button>
+      </div>
+    </form>
     <Button secondary>
       Customize Campaign Tracking
     </Button>
