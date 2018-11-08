07c4236075739cc6763bb2f7d267adc160c87b03
Hamish Macpherson <hamstu@gmail.com>
2018-11-08 16:55:27 +0000

    refactor hash checking

--- packages/modals/middleware.js
+++ packages/modals/middleware.js
@@ -1,28 +1,13 @@
 import { actionTypes } from '@bufferapp/async-data-fetch';
 import { actions } from './reducer';
-
-const checkHashShowUpgradeModal = () => {
-  const hash = window.location.hash;
-  let showUpgradeModal = false;
-  let source = null;
-  if (hash.indexOf('#upgrade-to-pro') === 0) {
-    showUpgradeModal = true;
-    // #show-upgrade-modal--profile_limit => 'profile_limit'
-    source = hash.split('--')[1] || 'unknown';
-  }
-  return {
-    showUpgradeModal,
-    source,
-  };
-};
+import { shouldShowModal, getSourceFromHash } from './util/window-hash';
 
 export default ({ dispatch }) => next => (action) => {
   next(action);
   switch (action.type) {
     case `user_${actionTypes.FETCH_SUCCESS}`: {
-      const { showUpgradeModal, source } = checkHashShowUpgradeModal();
-      if (showUpgradeModal) {
-        dispatch(actions.showUpgradeModal({ source }));
+      if (shouldShowModal()) {
+        dispatch(actions.showUpgradeModal({ source: getSourceFromHash() }));
       }
       break;
     }
