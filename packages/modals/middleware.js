2bb7bd788369736171d75559a8b70415d6d9e0b7
Hamish Macpherson <hamstu@gmail.com>
2018-11-08 11:47:23 +0000

    extract upgrade modal source from url hash

--- packages/modals/middleware.js
+++ packages/modals/middleware.js
@@ -1,16 +1,34 @@
+import { actionTypes } from '@bufferapp/async-data-fetch';
 import { actions } from './reducer';
+
+const checkHashShowUpgradeModal = () => {
+  const hash = window.location.hash;
+  let showUpgradeModal = false;
+  let source = null;
+  if (hash.indexOf('#upgrade-to-pro') === 0) {
+    showUpgradeModal = true;
+    // #show-upgrade-modal--profile_limit => 'profile_limit'
+    source = hash.split('--')[1] || 'unknown';
+  }
+  return {
+    showUpgradeModal,
+    source,
+  };
+};
 
 export default ({ dispatch }) => next => (action) => {
   next(action);
   switch (action.type) {
-    case 'APP_INIT':
-      if (window.location.hash === '#upgrade-to-pro') {
-        dispatch(actions.showUpgradeModal());
+    case `user_${actionTypes.FETCH_SUCCESS}`: {
+      const { showUpgradeModal, source } = checkHashShowUpgradeModal();
+      if (showUpgradeModal) {
+        dispatch(actions.showUpgradeModal({ source }));
       }
       break;
+    }
     case 'COMPOSER_EVENT':
       if (action.eventType === 'show-upgrade-modal') {
-        dispatch(actions.showUpgradeModal());
+        dispatch(actions.showUpgradeModal({ source: 'queue_limit' }));
       }
       break;
     default:
