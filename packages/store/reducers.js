07f8514c23d6dc751267cb575b743d5a2d367124
AL <amyleeannebennett@gmail.com>
2018-11-18 14:57:49 -0600

    Add datepicker reducer and dependencies needed for store

--- packages/store/reducers.js
+++ packages/store/reducers.js
@@ -24,6 +24,7 @@ import { reducer as changePasswordReducer } from '@bufferapp/change-password';
 import { reducer as manageAppsReducer } from '@bufferapp/manage-apps-extras';
 import { reducer as twoFactorAuthReducer } from '@bufferapp/publish-two-factor-auth';
 import { reducer as closeAccountReducer } from '@bufferapp/close-account';
+import { reducer as datePickerReducer } from '@bufferapp/analyze-date-picker';
 
 export default combineReducers({
   form: formReducer,
@@ -51,4 +52,5 @@ export default combineReducers({
   drafts: draftsReducer,
   generalSettings: generalSettingsReducer,
   postingSchedule: postingScheduleReducer,
+  date: datePickerReducer,
 });
