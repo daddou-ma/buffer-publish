5439fcb0f09c0b8b5c9e0b85d4ad1c029f0b66a8
AL <amyleeannebennett@gmail.com>
2018-11-18 15:00:16 -0600

    Add structure for analytics package

---
+++ packages/analytics/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import DatePicker from '@bufferapp/analyze-date-picker';
+
+const Header = () => (
+  <div>
+    <DatePicker />
+  </div>
+);
+
+export default Header;
---
+++ packages/analytics/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Header from './components/Header';
+
+const Analytics = () => (
+  <div>
+    <Header />
+  </div>
+);
+
+export default Analytics;
