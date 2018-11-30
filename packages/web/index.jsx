bdd1d7bd7fe9c880552c882a877b2e3e1e173e0f
Hamish Macpherson <hamstu@gmail.com>
2018-11-30 09:52:52 -0800

    Add bugsnag handling in React app

--- packages/web/index.jsx
+++ packages/web/index.jsx
@@ -3,8 +3,24 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter as Router } from 'react-router-redux';
 
+import bugsnag from '@bugsnag/js';
+import bugsnagReact from '@bugsnag/plugin-react';
+
 import createStore, { history } from '@bufferapp/publish-store';
 import App from './components/App';
+
+/**
+ * Setup Bugsnag
+ * https://docs.bugsnag.com/platforms/javascript/react/#installation
+ */
+let ErrorBoundary;
+if (window._bugsnagConfig) {
+  window.bugsnagClient = bugsnag(window._bugsnagConfig);
+  window.bugsnagClient.use(bugsnagReact, React);
+  ErrorBoundary = window.bugsnagClient.getPlugin('react');
+} else {
+  ErrorBoundary = React.Fragment; // no-op component
+}
 
 const store = createStore();
 
@@ -13,10 +29,12 @@ store.dispatch({
 });
 
 render(
-  <Provider store={store}>
-    <Router history={history}>
-      <App />
-    </Router>
-  </Provider>,
+  <ErrorBoundary>
+    <Provider store={store}>
+      <Router history={history}>
+        <App />
+      </Router>
+    </Provider>
+  </ErrorBoundary>,
   document.getElementById('root'),
 );
