e407ea7a89c69232e57938aa5ad3c9a4e5d1e66a
AL <amyleeannebennett@gmail.com>
2018-12-03 09:03:07 -0600

    Add posts-table to analytics package

--- packages/analytics/index.js
+++ packages/analytics/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AverageTable from '@bufferapp/average-table';
 import CompareChart from '@bufferapp/compare-chart';
+import PostsTable from '@bufferapp/posts-table';
 import SummaryTable from '@bufferapp/summary-table';
 import Toolbar from './components/Toolbar';
 import './analytics.css';
@@ -11,6 +12,7 @@ const Analytics = () => (
     <SummaryTable />
     <AverageTable />
     <CompareChart />
+    <PostsTable />
   </div>
 );
 
