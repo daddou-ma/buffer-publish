2590b325bc0255d9b6be75f595dcbf02dba669b0
Hamish Macpherson <hamstu@gmail.com>
2018-12-03 12:24:48 -0800

    put errorboundary in separate file

--- packages/web/index.jsx
+++ packages/web/index.jsx
@@ -3,30 +3,17 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter as Router } from 'react-router-redux';
 
-import bugsnag from '@bugsnag/js';
-import bugsnagReact from '@bugsnag/plugin-react';
-
 import createStore, { history } from '@bufferapp/publish-store';
 import App from './components/App';
-
-/**
- * Setup Bugsnag
- * https://docs.bugsnag.com/platforms/javascript/react/#installation
- */
-let ErrorBoundary;
-if (window._bugsnagConfig) {
-  window.bugsnagClient = bugsnag(window._bugsnagConfig);
-  window.bugsnagClient.use(bugsnagReact, React);
-  ErrorBoundary = window.bugsnagClient.getPlugin('react');
-} else {
-  ErrorBoundary = React.Fragment; // no-op component
-}
+import getErrorBoundary from './components/ErrorBoundary';
 
 const store = createStore();
 
 store.dispatch({
   type: 'APP_INIT',
 });
+
+const ErrorBoundary = getErrorBoundary();
 
 render(
   <ErrorBoundary>
