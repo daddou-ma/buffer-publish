e104e7cc9a0b4c0f4d853ddd31731203daff33c6
Hamish Macpherson <hamstu@gmail.com>
2018-12-03 13:49:44 -0800

    use 0.2.0 uploader and new test folder

--- pre-build.sh
+++ pre-build.sh
@@ -19,12 +19,12 @@ yarn run bugsnag:release
 
 yarn run build
 
-UPLOADER="https://github.com/bufferapp/buffer-static-upload/releases/download/0.1.0/buffer-static-upload-`uname -s`"
+UPLOADER="https://github.com/bufferapp/buffer-static-upload/releases/download/0.2.0/buffer-static-upload-`uname -s`"
 curl -L $UPLOADER > ./buffer-static-upload
 chmod +x ./buffer-static-upload
 
 FILES="vendor.js,bundle.js,bundle.css,*.map"
-./buffer-static-upload -files "$FILES" -bucket static.buffer.com -dir publish
+./buffer-static-upload -files "$FILES" -bucket static.buffer.com -dir publish-dev
 
 echo "STATIC ASSETS:"
 cat ./staticAssets.json
