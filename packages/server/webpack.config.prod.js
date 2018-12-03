4f9b958579f5cf6f55038570d7203cebf964fb75
Hamish Macpherson <hamstu@gmail.com>
2018-12-03 15:56:02 -0800

    validate release data and move back to normal publish dir in s3

--- packages/server/webpack.config.prod.js
+++ packages/server/webpack.config.prod.js
@@ -10,10 +10,15 @@ const merged = merge(common, {
     nodeEnv: 'production',
   },
   plugins: [
+    // Create source maps that have the [hash] in them.
+    // Since there's no replacement string for the extension
+    // (e.g., '[ext]') we have to have one for JS and another
+    // for CSS.
     new webpack.SourceMapDevToolPlugin({
       filename: '[name].[hash].js.map',
       exclude: ['bundle.css'],
     }),
+    // Create source maps for the CSS
     new webpack.SourceMapDevToolPlugin({
       filename: '[name].[hash].css.map',
       test: ['bundle.css'],
