64dd7e18818263a49cb4a48af43d240f206631cb
Maya Uribe <mariale.uribe@gmail.com>
2018-12-04 17:16:11 +0100

    Removing unused function.

--- packages/tabs/utils/index.js
+++ packages/tabs/utils/index.js
@@ -1,14 +1,3 @@
-const openPopup = (url, title, w, h) => {
-  const left = (screen.width / 2) - (w / 2);
-  const top = (screen.height / 2) - (h / 2);
-  const newWindow = window.open(url, title, `scrollbars=yes, width=${w}, height=${h}, top=${top} left=${left}`);
-
-  // Puts focus on the newWindow
-  if (window.focus) {
-    newWindow.focus();
-  }
-};
-
 const getBaseURL = () => {
   return window.location.hostname === 'publish.local.buffer.com' ? 'https://local.buffer.com' : 'https://buffer.com';
 };
