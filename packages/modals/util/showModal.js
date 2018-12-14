d6854fe140e6af9723ef8cad4962ecce29335afe
Hamish Macpherson <hamstu@gmail.com>
2018-12-14 14:48:03 -0800

    use query params to trigger modals in app

---
+++ packages/modals/util/showModal.js
@@ -0,0 +1,26 @@
+/**
+ * See the code in `server/index.js` to see how the modal
+ * to show on load is passed via a query param.
+ */
+
+const getShowModalKey = () => {
+  if (window._showModal && window._showModal.key) {
+    return window._showModal.key;
+  }
+  return '';
+};
+
+export const shouldShowWelcomeModal = () =>
+  getShowModalKey() === 'welcome-modal-1';
+
+export const shouldShowUpgradeModal = () =>
+  getShowModalKey().indexOf('upgrade-to-pro') === 0;
+
+/**
+ * Extracts a source from the modal key in the URL
+ * 'upgrade-to-pro--profile_limit' => 'profile_limit'
+ */
+export const getSourceFromKey = () => {
+  const key = getShowModalKey();
+  return key.split('--')[1] || 'unknown';
+};
--- packages/upgrade-modal/middleware.js
+++ packages/upgrade-modal/middleware.js
@@ -6,7 +6,18 @@ import { actionTypes } from './reducer';
 export default ({ getState, dispatch }) => next => (action) => { // eslint-disable-line
   const card = getState().upgradeModal.card;
   const source = getState().upgradeModal.source;
-  const userId = getState().appSidebar.user.id;
+
+  /**
+   * Pulling the User ID from the Bugsnag global feels wrong,
+   * but this modal can be triggered immediately on load and
+   * before the user data is loaded, so it's the safest.
+   *
+   * @todo: Have `server/index.js` put the user ID somewhere
+   * more generic so we can pull the ID from there.
+   */
+  const userId = window._bugsnagConfig
+    ? window._bugsnagConfig.user.id
+    : '';
 
   next(action);
 
