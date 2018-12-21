834f798beff3991403bcfe2fafb32f31a40f3d79
AL <amyleeannebennett@gmail.com>
2018-12-21 16:23:11 -0600

    Change condition to include undefined and handle subprofile being null

--- packages/shared-components/Post/RenderPostMetaBar/index.jsx
+++ packages/shared-components/Post/RenderPostMetaBar/index.jsx
@@ -10,23 +10,26 @@ const RenderPostMetaBar = ({
   subprofileID,
   subprofiles,
   dragging,
+  isSent,
 }) => {
-  if (profileService === 'instagram' && locationName !== null) {
+  if (profileService === 'instagram' && locationName) {
     return (
       <InstagramPostMetaBar
         dragging={dragging}
         locationName={locationName}
+        isSent={isSent}
       />
     );
-  } else if (profileService === 'pinterest' && subprofileID !== null) {
+  } else if (profileService === 'pinterest' && subprofileID) {
     /*  having a subprofileID is required, sourceUrl is not */
     const subprofile = subprofiles.find((profile => profile.id === subprofileID));
     return (
       <PinterestPostMetaBar
         dragging={dragging}
-        boardName={subprofile.name}
-        boardAvatarUrl={subprofile.avatar}
+        boardName={subprofile ? subprofile.name : null}
+        boardAvatarUrl={subprofile ? subprofile.avatar : null}
         sourceUrl={sourceUrl}
+        isSent={isSent}
       />
     );
   }
