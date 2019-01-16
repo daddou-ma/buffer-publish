d3722a91007c5a425a8ebc2cf0c713c464de8fee
Hamish Macpherson <hamstu@gmail.com>
2019-01-16 14:03:48 -0800

    hotfix; temporarily pause tracking for composer

--- packages/server/lib/composerAjaxBuffermetrics.js
+++ packages/server/lib/composerAjaxBuffermetrics.js
@@ -1,25 +1,31 @@
 module.exports = (req, res) => {
-  const { values } = req.body;
-
-  values.forEach((event) => {
-    const scopes = event.value;
-    const metadata = event.extra_data;
-    const indexOfMc = scopes.indexOf('multiple-composers');
-
-    if (indexOfMc) {
-      // We can only send one action with our new metrics
-      // so we'll concat anything after the mc part
-      let action = scopes.splice(indexOfMc + 1);
-      action = action.join('-');
-
-      req.buffermetrics.trackAction({
-        application: 'publish',
-        location: 'composer',
-        action,
-        metadata,
-      });
-    }
-  });
-
+  /**
+   * Sending success for now until we fix issues with the
+   * tracking code below.
+   */
   res.send('success');
+
+  // const { values } = req.body;
+
+  // values.forEach((event) => {
+  //   const scopes = event.value;
+  //   const metadata = event.extra_data;
+  //   const indexOfMc = scopes.indexOf('multiple-composers');
+
+  //   if (indexOfMc) {
+  //     // We can only send one action with our new metrics
+  //     // so we'll concat anything after the mc part
+  //     let action = scopes.splice(indexOfMc + 1);
+  //     action = action.join('-');
+
+  //     req.buffermetrics.trackAction({
+  //       application: 'publish',
+  //       location: 'composer',
+  //       action,
+  //       metadata,
+  //     });
+  //   }
+  // });
+
+  // res.send('success');
 };
