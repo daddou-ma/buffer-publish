d3e9fbd07ce5bdcc7cc44801ae3e7a1fa582f6a6
Maya Uribe <mariale.uribe@gmail.com>
2019-02-01 18:24:49 +0100

    Adding tests for general settings reducer.

--- packages/general-settings/reducer.test.js
+++ packages/general-settings/reducer.test.js
@@ -81,4 +81,84 @@ describe('reducer', () => {
     expect(reducer(undefined, action))
       .toEqual(stateAfter);
   });
+
+  it('should save GA Tracking settings', () => {
+    const stateAfter = {
+      isInstagramProfile: false,
+      isInstagramBusiness: false,
+      googleAnalyticsIsEnabled: false,
+      profileId: null,
+      showGACustomizationForm: false,
+    };
+    const action = {
+      type: `saveGATrackingSettings_${dataFetchActionTypes.FETCH_SUCCESS}`,
+      profile: {
+        showGACustomizationForm: false,
+        result: 'Okay, your Campaign tracking parameters have been updated.',
+      },
+    };
+    deepFreeze(action);
+    expect(reducer(undefined, action))
+      .toEqual(stateAfter);
+  });
+
+  it('should set utm campaign value', () => {
+    const stateAfter = {
+      isInstagramProfile: false,
+      isInstagramBusiness: false,
+      googleAnalyticsIsEnabled: false,
+      profileId: null,
+      showGACustomizationForm: false,
+    };
+    const action = {
+      type: actionTypes.SET_UTM_CAMPAIGN,
+      profile: {
+        showGACustomizationForm: true,
+        utmCampaign: 'Campaign',
+      },
+    };
+    deepFreeze(action);
+    expect(reducer(undefined, action))
+      .toEqual(stateAfter);
+  });
+
+  it('should set utm source value', () => {
+    const stateAfter = {
+      isInstagramProfile: false,
+      isInstagramBusiness: false,
+      googleAnalyticsIsEnabled: false,
+      profileId: null,
+      showGACustomizationForm: false,
+    };
+    const action = {
+      type: actionTypes.SET_UTM_SOURCE,
+      profile: {
+        showGACustomizationForm: true,
+        utmSource: 'Source',
+      },
+    };
+    deepFreeze(action);
+    expect(reducer(undefined, action))
+      .toEqual(stateAfter);
+  });
+
+  it('should set utm medium value', () => {
+    const stateAfter = {
+      isInstagramProfile: false,
+      isInstagramBusiness: false,
+      googleAnalyticsIsEnabled: false,
+      profileId: null,
+      showGACustomizationForm: false,
+    };
+    const action = {
+      type: actionTypes.SET_UTM_MEDIUM,
+      profile: {
+        showGACustomizationForm: true,
+        utmMedium: 'Medium',
+      },
+    };
+    deepFreeze(action);
+    expect(reducer(undefined, action))
+      .toEqual(stateAfter);
+  });
 });
