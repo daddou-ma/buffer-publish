59ab92aa1f1d6dbad11140c5b7be0b5e31c51958
Maya Uribe <mariale.uribe@gmail.com>
2019-02-11 16:14:00 +0100

    Updating stylings for steal profile modal.

--- packages/steal-profile-modal/components/StealProfileModal/index.jsx
+++ packages/steal-profile-modal/components/StealProfileModal/index.jsx
@@ -5,8 +5,23 @@ import {
   Card,
   Text,
   Button,
+  Link,
   NotificationIcon,
 } from '@bufferapp/components';
+
+const titleStyle = {
+  display: 'flex',
+};
+
+const iconStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  paddingRight: '0.5rem',
+};
+
+const contentStyle = {
+  marginTop: '0.8rem',
+};
 
 const StealProfileModal = ({
   translations,
@@ -15,31 +30,52 @@ const StealProfileModal = ({
   email,
 }) => (
   <div style={{ position: 'fixed', zIndex: '3000' }}>
-    <Popover>
-      <Card>
-        <div style={{ maxWidth: '380px', textAlign: 'center', margin: '10px 40px' }}>
-          <Text size="large" weight="medium" color="black">
-            <NotificationIcon />
-            {translations.headline1} {stealProfileUsername} {translations.headline2}
-          </Text>
-          <div style={{ margin: '16px 0 24px' }}>
-            <Text>{translations.body1}</Text>
-          </div>
-          <div style={{ margin: '16px 0 24px' }}>
-            <Text>
-              {translations.body2}
-              {stealProfileUsername}
-              {translations.body3}
-              {email}
-              {translations.body4}
+    <Popover onOverlayClick={hideModal}>
+      <Card reducedPadding>
+        <div style={{ maxWidth: '580px', margin: '10px 20px' }}>
+          <div style={titleStyle}>
+            <span style={iconStyle}>
+              <NotificationIcon size={12} />
+            </span>
+            <Text
+              color={'outerSpace'}
+              weight={'medium'}
+            >
+              {translations.headline1} {stealProfileUsername} {translations.headline2}
             </Text>
           </div>
-          <div style={{ margin: '16px 0 24px' }}>
-            <Text>
-              {translations.notSureWhere} {translations.getInTouch}
+          <div style={contentStyle}>
+            <Text size={'mini'}>
+              {translations.body1}
             </Text>
           </div>
-          <Button onClick={hideModal} large>{translations.takeMeBack}</Button>
+          <div style={contentStyle}>
+            <Text size={'mini'} weight={'medium'}>
+              {
+                `${translations.body2} ${stealProfileUsername} ${translations.body3}
+                 ${email} ${translations.body4}
+                `
+              }
+            </Text>
+          </div>
+          <div style={contentStyle}>
+            <Text size={'mini'} weight={'medium'}>
+              {translations.notSureWhere}&nbsp;
+              <Link newTab href="https://buffer.com/support">
+                {translations.getInTouch}
+              </Link>
+            </Text>
+          </div>
+          <form style={{ paddingTop: '1.5rem', display: 'flex', justifyContent: 'center' }}>
+            <Button
+              onClick={hideModal}
+              large
+            >
+              <Text size={'mini'} weight={'medium'} color={'white'}>
+                {translations.takeMeBack}
+              </Text>
+            </Button>
+          </form>
         </div>
       </Card>
     </Popover>
