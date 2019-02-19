ebd706fb139bc66687c8824f4eec75058c69d576
AL <amyleeannebennett@gmail.com>
2019-02-18 18:43:12 -0600

    Update snapshots with TestContextContainer decorator

--- packages/profile-sidebar/components/ProfileList/story.jsx
+++ packages/profile-sidebar/components/ProfileList/story.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { checkA11y } from 'storybook-addon-a11y';
+import TestBackend from 'react-dnd-test-backend';
+import { DragDropContext } from 'react-dnd';
 import ProfileList from './index';
 import profiles from '../../mockData/profiles';
+/* eslint-disable react/prop-types */
+class _TestContextContainer extends Component { // eslint-disable-line
+  render() {
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+const TestContextContainer = DragDropContext(TestBackend)(_TestContextContainer);
 
 storiesOf('ProfileList', module)
   .addDecorator(checkA11y)
+  .addDecorator(getStory => <TestContextContainer>{getStory()}</TestContextContainer>)
   .add('should display a list of profiles', () => (
     <ProfileList
       profiles={profiles}
