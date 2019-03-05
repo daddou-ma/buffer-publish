1c2cee85a8b0b941df3882087a92a93893217a81
Hamish Macpherson <hamstu@gmail.com>
2019-03-05 14:44:37 -0800

    add testing shortcut command (#624)

---
+++ scripts/test-package.sh
@@ -0,0 +1,19 @@
+if [[ $# -eq 0 ]] ; then
+  echo
+  echo 'Please pass the name of the package folder as the first argument.'
+  echo
+  exit 0
+fi
+
+# remove the ./ prefix, if present
+path=${1#"./"}
+
+echo "Starting test suite for ${path}"
+
+yarn run jest \
+  --watch \
+  --verbose=false \
+  --testPathPattern $path \
+  --coverage \
+  --collectCoverageFrom='["'"$path"'/**/*.jsx", "'"$path"'/**/*.js"]' \
+  -u
