9bf7ebd13d36d7c0601a83523d8ca9264dc92f13
AL <amyleeannebennett@gmail.com>
2019-03-04 18:16:30 -0600

    Change reconnect tab to button

--- packages/tabs/components/TabNavigation/index.jsx
+++ packages/tabs/components/TabNavigation/index.jsx
@@ -18,103 +18,135 @@ const upgradeCtaStyle = {
   right: 0,
 };
 
-const TabNavigation = ({
-  features,
-  isBusinessAccount,
-  isManager,
-  selectedTabId,
-  selectedChildTabId,
-  onTabClick,
-  onChildTabClick,
-  shouldShowUpgradeCta,
-  shouldShowNestedSettingsTab,
-  onUpgradeButtonClick,
-  profileId,
-  isLockedProfile,
-  isInstagramProfile,
-}) => {
-  const selectedChildTab = selectedChildTabId || 'general-settings';
-  return (
-    /* wrapper div with "tabs" id necessary as a selector
-    for a11y focus after selecting profile in sidebar */
-    <div id="tabs" style={{ paddingLeft: '0.5rem' }}>
-      <Tabs
-        selectedTabId={selectedTabId}
-        onTabClick={onTabClick}
-      >
-        <Tab tabId={'queue'}>Queue</Tab>
-        <Tab tabId={'sent'}>Sent Posts</Tab>
-        {isInstagramProfile &&
-          <Tab tabId={'pastReminders'}>Past Reminders</Tab>
-        }
-        {/* Pro and up users or Team Members */}
-        {(!features.isFreeUser() || isBusinessAccount) &&
-          <Tab tabId={'analytics'}>Analytics</Tab>
-        }
-        {/* Team Members who are Managers */}
-        {isBusinessAccount && isManager &&
-          <Tab tabId={'awaitingApproval'}>Awaiting Approval</Tab>
-        }
-        {/* Team Members who are Contributors */}
-        {isBusinessAccount && !isManager &&
-          <Tab tabId={'pendingApproval'}>Pending Approval</Tab>
-        }
-        {/* Pro and up users or Team Members */}
-        {(!features.isFreeUser() || isBusinessAccount) &&
-          <Tab tabId={'drafts'}>Drafts</Tab>
-        }
-        <FeatureLoader supportedFeatures={'b4b_calendar'}>
-          <Tab tabId={'b4bCalendar'} onClick={() => openCalendarWindow(profileId)}>
-            Calendar
-          </Tab>
-        </FeatureLoader>
-        <Tab tabId={'settings'}>Settings</Tab>
-      </Tabs>
-      {shouldShowUpgradeCta &&
-        <div style={upgradeCtaStyle}>
-          <div style={{ paddingRight: '10px', display: 'inline-block' }}>
-            <Text size="mini">
-              Want to see more from Buffer?
-          </Text>
-          </div>
-          <Button
-            secondary
-            onClick={(e) => {
-              e.preventDefault();
-              onUpgradeButtonClick('pro');
-            }}
-          >
-            Upgrade to Pro
-            </Button>
-        </div>
-      }
-      <FeatureLoader supportedPlans={'pro'}>
-        <div style={upgradeCtaStyle}>
-          <Button
-            secondary
-            onClick={(e) => {
-              e.preventDefault();
-              onUpgradeButtonClick('b4b');
-            }}
-          >
-            Learn about Buffer for Business
-            </Button>
-        </div>
-      </FeatureLoader>
-      {shouldShowNestedSettingsTab && !isLockedProfile &&
-        <Tabs
-          selectedTabId={selectedChildTab}
-          onTabClick={onChildTabClick}
-          secondary
-        >
-          <Tab tabId={'general-settings'}>General</Tab>
-          <Tab tabId={'posting-schedule'}>Posting Schedule</Tab>
-          <Tab tabId={'reconnect'}>Reconnect</Tab>
-        </Tabs>
-      }
-    </div>
-  );
+const reconnectProfile = () => {
+  if (window.location.hostname === 'publish.local.buffer.com') {
+    window.location.assign('https://local.buffer.com/manage/');
+  } else {
+    window.location.assign('https://buffer.com/manage/');
+  }
 };
+
+class TabNavigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: false };
+  }
+
+
+  render () {
+    const {
+      features,
+      isBusinessAccount,
+      isManager,
+      selectedTabId,
+      selectedChildTabId,
+      onTabClick,
+      onChildTabClick,
+      shouldShowUpgradeCta,
+      shouldShowNestedSettingsTab,
+      onUpgradeButtonClick,
+      profileId,
+      isLockedProfile,
+      isInstagramProfile,
+    } = this.props;
+
+    const selectedChildTab = selectedChildTabId || 'general-settings';
+    return (
+      /* wrapper div with "tabs" id necessary as a selector
+      for a11y focus after selecting profile in sidebar */
+      <div id="tabs" style={{ paddingLeft: '0.5rem' }}>
+        <Tabs
+          selectedTabId={selectedTabId}
+          onTabClick={onTabClick}
+        >
+          <Tab tabId={'queue'}>Queue</Tab>
+          <Tab tabId={'sent'}>Sent Posts</Tab>
+          {isInstagramProfile &&
+            <Tab tabId={'pastReminders'}>Past Reminders</Tab>
+          }
+          {/* Pro and up users or Team Members */}
+          {(!features.isFreeUser() || isBusinessAccount) &&
+            <Tab tabId={'analytics'}>Analytics</Tab>
+          }
+          {/* Team Members who are Managers */}
+          {isBusinessAccount && isManager &&
+            <Tab tabId={'awaitingApproval'}>Awaiting Approval</Tab>
+          }
+          {/* Team Members who are Contributors */}
+          {isBusinessAccount && !isManager &&
+            <Tab tabId={'pendingApproval'}>Pending Approval</Tab>
+          }
+          {/* Pro and up users or Team Members */}
+          {(!features.isFreeUser() || isBusinessAccount) &&
+            <Tab tabId={'drafts'}>Drafts</Tab>
+          }
+          <FeatureLoader supportedFeatures={'b4b_calendar'}>
+            <Tab tabId={'b4bCalendar'} onClick={() => openCalendarWindow(profileId)}>
+              Calendar
+            </Tab>
+          </FeatureLoader>
+          <Tab tabId={'settings'}>Settings</Tab>
+        </Tabs>
+        {shouldShowUpgradeCta &&
+          <div style={upgradeCtaStyle}>
+            <div style={{ paddingRight: '10px', display: 'inline-block' }}>
+              <Text size="mini">
+                Want to see more from Buffer?
+            </Text>
+            </div>
+            <Button
+              secondary
+              onClick={(e) => {
+                e.preventDefault();
+                onUpgradeButtonClick('pro');
+              }}
+            >
+              Upgrade to Pro
+              </Button>
+          </div>
+        }
+        <FeatureLoader supportedPlans={'pro'}>
+          <div style={upgradeCtaStyle}>
+            <Button
+              secondary
+              onClick={(e) => {
+                e.preventDefault();
+                onUpgradeButtonClick('b4b');
+              }}
+            >
+              Learn about Buffer for Business
+              </Button>
+          </div>
+        </FeatureLoader>
+        {shouldShowNestedSettingsTab && !isLockedProfile &&
+          <div style={{ maxWidth: '864px' }}>
+            <Tabs
+              selectedTabId={selectedChildTab}
+              onTabClick={onChildTabClick}
+              secondary
+            >
+              <Tab tabId={'general-settings'}>General</Tab>
+              <Tab tabId={'posting-schedule'}>Posting Schedule</Tab>
+              <div style={{ float: 'right', padding: '12px 13px' }}>
+                <Button
+                  secondary
+                  small
+                  onClick={(e) => {
+                    e.preventDefault();
+                    this.setState({ loading: true });
+                    reconnectProfile();
+                  }}
+                >
+                  { this.state.loading ? 'Reconnecting…' : 'Reconnect' }
+                </Button>
+              </div>
+            </Tabs>
+          </div>
+        }
+      </div>
+    );
+  }
+}
 
 TabNavigation.defaultProps = {
   shouldShowUpgradeCta: false,
