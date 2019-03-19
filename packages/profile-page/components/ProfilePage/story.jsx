ec24a202d69b06d908a0240ea9a8486ea2698d16
Ana Almeida <anafilipadealmeida@gmail.com>
2019-03-19 18:32:41 +0000

    Update snapshots

--- packages/profile-page/components/ProfilePage/story.jsx
+++ packages/profile-page/components/ProfilePage/story.jsx
@@ -51,12 +51,6 @@ storiesOf('ProfilePage', module)
       history={stubbedHistory}
     />
   ))
-
-  /* Test not working, temporarily dismissed. Invariant Violation: Element type is invalid:
-  expected a string (for built-in components) or a class/function (for composite components) but
-  got: undefined. You likely forgot to export your component from the file it's defined in, or
-  you might have mixed up default and named imports. Check the render method of `FeatureLoader` */
-
   .add('should render general settings', () => (
     <ProfilePage
       match={{
