194a4dea1da69a8afd6f8c55b9070a1b2b3bc4e0
AL <amyleeannebennett@gmail.com>
2019-03-26 10:34:14 -0500

    Add tests to sent middleware

--- packages/sent/middleware.test.js
+++ packages/sent/middleware.test.js
@@ -1,8 +1,83 @@
+import moment from 'moment-timezone';
+import { actionTypes as profileActionTypes } from '@bufferapp/publish-profile-sidebar';
+import {
+  actions as dataFetchActions,
+} from '@bufferapp/async-data-fetch';
 import middleware from './middleware';
 
+const getStateWithPaidUser = () => ({
+  appSidebar: {
+    user: {
+      is_free_user: false,
+    },
+  },
+  profileSidebar: {
+    selectedProfile: {
+      business: true,
+    },
+  },
+});
+
+const getStateWithFreeUser = () => ({
+  appSidebar: {
+    user: {
+      is_free_user: true,
+    },
+  },
+  profileSidebar: {
+    selectedProfile: {
+      business: false,
+    },
+  },
+});
+
 describe('middleware', () => {
+  const next = jest.fn();
+  const dispatch = jest.fn();
   it('should export middleware', () => {
     expect(middleware)
       .toBeDefined();
+  });
+
+  it('should fetch sentPosts for paid users', () => {
+    const action = {
+      type: profileActionTypes.SELECT_PROFILE,
+      profile: {
+        id: 'id1',
+      },
+    };
+    middleware({ dispatch, getState: getStateWithPaidUser })(next)(action);
+    expect(next)
+    .toBeCalledWith(action);
+    expect(dispatch)
+    .toBeCalledWith(dataFetchActions.fetch({
+      name: 'sentPosts',
+      args: {
+        profileId: action.profile.id,
+        isFetchingMore: false,
+        since: moment().subtract(10, 'years').unix(),
+      },
+    }));
+  });
+
+  it('should fetch sentPosts for free users', () => {
+    const action = {
+      type: profileActionTypes.SELECT_PROFILE,
+      profile: {
+        id: 'id1',
+      },
+    };
+    middleware({ dispatch, getState: getStateWithFreeUser })(next)(action);
+    expect(next)
+    .toBeCalledWith(action);
+    expect(dispatch)
+    .toBeCalledWith(dataFetchActions.fetch({
+      name: 'sentPosts',
+      args: {
+        profileId: action.profile.id,
+        isFetchingMore: false,
+        since: null,
+      },
+    }));
   });
 });
