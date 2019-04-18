663fe6952ba50e968c3157f093c6ac18202a4fd1
AL <amyleeannebennett@gmail.com>
2019-04-17 17:44:45 -1000

    Update queue state after shuffle

--- packages/general-settings/reducer.js
+++ packages/general-settings/reducer.js
@@ -1,4 +1,5 @@
 import { actionTypes as profileActionTypes } from '@bufferapp/publish-profile-sidebar';
+import { actionTypes as queueActionTypes } from '@bufferapp/publish-queue';
 import { actionTypes as dataFetchActionTypes } from '@bufferapp/async-data-fetch';
 import keyWrapper from '@bufferapp/keywrapper';
 
