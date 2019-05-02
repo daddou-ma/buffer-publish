eaa8af8174773986226c0aa07fc1e9d87bd1481b
AL <amyleeannebennett@gmail.com>
2019-05-02 12:03:33 -0500

    Add rpc cancelTrial package

---
+++ packages/server/rpc/cancelTrial/index.js
@@ -0,0 +1,27 @@
+const { method, createError } = require('@bufferapp/buffer-rpc');
+const rp = require('request-promise');
+
+module.exports = method(
+  'cancelTrial',
+  'cancel trial',
+  async ({ token }, { session }) => {
+    let result;
+    try {
+      result = await rp({
+        uri: `${process.env.API_ADDR}/1/billing/cancel-trial.json`,
+        method: 'POST',
+        strictSSL: process.env.NODE_ENV !== 'development',
+        json: true,
+        form: {
+          access_token: session.publish.accessToken,
+          product: 'publish',
+        },
+      });
+    } catch (response) {
+      throw createError({
+        message: response.error,
+      });
+    }
+    return result;
+  },
+);
