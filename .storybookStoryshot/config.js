f72f9e78842189b96a6d8ec04195a29053efda6c
Jose M. Gilgado <jm.gilgado@gmail.com>
2019-05-20 16:46:38 +0200

    Only add tests from shared-components

--- .storybookStoryshot/config.js
+++ .storybookStoryshot/config.js
@@ -1,6 +1,6 @@
 import 'babel-polyfill';
 import { configure } from '@storybook/react'
 
-const req = require.context('../packages', true, /components\/.*\/*story\.jsx$/)
-const loadStories = () => req.keys().forEach(req)
-configure(loadStories, module)
+const req = require.context('../packages/shared-components', true, /.*\/*story\.jsx$/);
+const loadStories = () => req.keys().forEach(req);
+configure(loadStories, module);
