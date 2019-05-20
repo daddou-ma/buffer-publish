6726e6a330816e4727cf4c1c051d093f4f0a8963
Jose M. Gilgado <jm.gilgado@gmail.com>
2019-05-20 18:16:36 +0200

    Add test for new third party client

--- .storybookStoryshot/config.js
+++ .storybookStoryshot/config.js
@@ -1,6 +1,7 @@
 import 'babel-polyfill';
 import { configure } from '@storybook/react'
 
+// Add all story.jsx files inside components directories except for the ones in node_modules
 const req = require.context('../packages', true, /^((?![\\/]node_modules[\\/]).)*components\/.*\/*story\.jsx$/)
 const loadStories = () => req.keys().forEach(req)
 configure(loadStories, module)
