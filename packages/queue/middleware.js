d7d8744e1e6834c87c1696f530b3a3f8bffadb24
AL <amyleeannebennett@gmail.com>
2019-06-05 10:53:20 +0200

    Remove console log

--- packages/queue/middleware.js
+++ packages/queue/middleware.js
@@ -95,7 +95,6 @@ export default ({ dispatch, getState }) => next => (action) => {
        * We also re-fetch the queue when a post is deleted
        * as this may have caused posts to change schedule
        */
-      console.log(action);
       dispatch(dataFetchActions.fetch({
         name: 'queuedPosts',
         args: {
