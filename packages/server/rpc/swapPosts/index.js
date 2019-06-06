1de6bc74296b346647eaaf76040b9f2d3bf54cdb
Maya Uribe <mariale.uribe@gmail.com>
2019-06-06 11:05:53 +0200

    Adding missing call in the middleware to save the swapped posts in the backend.

---
+++ packages/server/rpc/swapPosts/index.js
@@ -0,0 +1,32 @@
+const { method, createError } = require('@bufferapp/buffer-rpc');
+const rp = require('request-promise');
+
+module.exports = method(
+  'swapPosts',
+  'handles changing posts time when swapped',
+  ({
+     updateSourceId,
+     sourcePinned,
+     sourceDueAt,
+     updateTargetId,
+     targetPinned,
+     targetDueAt,
+   }, { session }) =>
+    rp({
+      uri: `${process.env.API_ADDR}/1/updates/${updateSourceId}/swap_posts.json`,
+      method: 'POST',
+      strictSSL: !(process.env.NODE_ENV === 'development'),
+      qs: {
+        access_token: session.publish.accessToken,
+        sourcePinned,
+        sourceDueAt,
+        updateTargetId,
+        targetPinned,
+        targetDueAt,
+      },
+    })
+      .then(() => 'OK')
+      .catch((err) => {
+        throw createError({ message: err.message });
+      }),
+);
