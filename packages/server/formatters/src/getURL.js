cd82a001ae3a5167bf6cb74e62b956ff6df6cb3a
anafilipadealmeida <anafilipadealmeida@gmail.com>
2019-06-17 15:12:11 +0100

    Add new url method and lint

--- packages/server/formatters/src/getURL.js
+++ packages/server/formatters/src/getURL.js
@@ -1,56 +1,68 @@
 module.exports = {
   getBaseURL: () => {
     if (window.location.hostname === 'publish.local.buffer.com') {
-      return 'local.buffer.com'
+      return 'local.buffer.com';
     }
-    return 'buffer.com'
+    return 'buffer.com';
   },
   getClassicBufferURL: () => {
     if (window.location.hostname === 'publish.local.buffer.com') {
-      return 'https://local.buffer.com/classic'
+      return 'https://local.buffer.com/classic';
     }
-    return 'https://buffer.com/classic'
+    return 'https://buffer.com/classic';
   },
   getBackToClassicNewPublishBufferURL: () => {
     if (window.location.hostname === 'publish.local.buffer.com') {
-      return 'https://local.buffer.com/back_publish'
+      return 'https://local.buffer.com/back_publish';
     }
-    return 'https://buffer.com/back_publish'
+    return 'https://buffer.com/back_publish';
+  },
+  getManageURL: () => {
+    if (window.location.hostname === 'publish.local.buffer.com') {
+      return 'https://local.buffer.com/manage';
+    }
+    return 'https://buffer.com/manage';
   },
   getConnectSocialAccountURL: () => {
     if (window.location.hostname === 'publish.local.buffer.com') {
-      return 'https://local.buffer.com/manage/accounts/connect'
+      return 'https://local.buffer.com/manage/accounts/connect';
     }
-    return 'https://buffer.com/manage/accounts/connect'
+    return 'https://buffer.com/manage/accounts/connect';
   },
   getManageSocialAccountURL: () => {
     if (window.location.hostname === 'publish.local.buffer.com') {
-      return 'https://local.buffer.com/manage/own'
+      return 'https://local.buffer.com/manage/own';
     }
-    return 'https://buffer.com/manage/own'
+    return 'https://buffer.com/manage/own';
   },
   getManageTeamURL: () => {
     if (window.location.hostname === 'publish.local.buffer.com') {
-      return 'https://local.buffer.com/manage/team-members'
+      return 'https://local.buffer.com/manage/team-members';
     }
-    return 'https://buffer.com/manage/team-members'
+    return 'https://buffer.com/manage/team-members';
   },
-  getInstagramDirectPostingURL: profileId => {
+  getInstagramDirectPostingURL: (profileId) => {
     if (window.location.hostname === 'publish.local.buffer.com') {
-      return `https://local.buffer.com/instagram/setup?profile_id=${profileId}`
+      return `https://local.buffer.com/instagram/setup?profile_id=${profileId}`;
     }
-    return `https://buffer.com/instagram/setup?profile_id=${profileId}`
+    return `https://buffer.com/instagram/setup?profile_id=${profileId}`;
   },
-  getConnectBitlyURL: profileId => {
+  getConnectBitlyURL: (profileId) => {
     if (window.location.hostname === 'publish.local.buffer.com') {
-      return `https://local.buffer.com/oauth/bitly/${profileId}/auth`
+      return `https://local.buffer.com/oauth/bitly/${profileId}/auth`;
     }
-    return `https://buffer.com/oauth/bitly/${profileId}/auth`
+    return `https://buffer.com/oauth/bitly/${profileId}/auth`;
   },
-  getDisconnectBitlyURL: profileId => {
+  getDisconnectBitlyURL: (profileId) => {
     if (window.location.hostname === 'publish.local.buffer.com') {
-      return `https://local.buffer.com/profile/disconnect_bitly_auth/${profileId}`
+      return `https://local.buffer.com/profile/disconnect_bitly_auth/${profileId}`;
     }
-    return `https://buffer.com/profile/disconnect_bitly_auth/${profileId}`
+    return `https://buffer.com/profile/disconnect_bitly_auth/${profileId}`;
   },
-}
+  getStartTrialURL: (trialType) => {
+    if (window.location.hostname === 'publish.local.buffer.com') {
+      return `https://local.buffer.com/billing/start-trial?trialType=${trialType}&next=https://publish.buffer.com`;
+    }
+    return `https://buffer.com/billing/start-trial?trialType=${trialType}&next=https://publish.buffer.com`;
+  },
+};
