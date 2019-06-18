f96bc69ed58423dface7f2f91b6159171a95167d
Jose M. Gilgado <jm.gilgado@gmail.com>
2019-06-18 13:46:04 +0200

    Add more tests for hashtag counter

--- packages/composer/composer/tests/lib/validation/HashtagCounter.test.js
+++ packages/composer/composer/tests/lib/validation/HashtagCounter.test.js
@@ -3,13 +3,35 @@ import countHashtagsInText from '../../../lib/validation/HashtagCounter';
 describe('countHashtagsInText', () => {
   describe('there are not any hashtags', () => {
     it('returns 0 if there are none', () => {
-      expect(countHashtagsInText('text without hashtags')).toBe(0);
+      const text = 'text without hashtags';
+      expect(countHashtagsInText(text)).toBe(0);
     });
   });
 
   describe('there are hashtags', () => {
-    it('returns number of hashtags if there are none', () => {
-      expect(countHashtagsInText('text without hashtags')).toBe(0);
+    it('returns number of simple hashtags', () => {
+      const text = 'test #one #two #three test';
+      expect(countHashtagsInText(text)).toBe(3);
+    });
+
+    it('returns number of hashtags together', () => {
+      const text = 'test #one#two#three #four test';
+      expect(countHashtagsInText(text)).toBe(4);
+    });
+
+    it('returns number of hashtags with emojis', () => {
+      const text = 'test #one#two #three #four #🙂 #🏯 test';
+      expect(countHashtagsInText(text)).toBe(6);
+    });
+
+    it('returns number of hashtags with UTF8 characters', () => {
+      const text = 'test #one #two #árbol #cayó';
+      expect(countHashtagsInText(text)).toBe(4);
+    });
+
+    it('returns number of hashtags with UTF8 characters together', () => {
+      const text = 'test #one#two#árbol#cayó';
+      expect(countHashtagsInText(text)).toBe(4);
     });
   });
 });
