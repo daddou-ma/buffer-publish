7d010daccf10e769279b9bbf2efed39b14496883
Karel McGrail <karel@buffer.com>
2019-06-20 17:18:13 +0100

    Adding help link with Shopgrid URL

--- packages/composer/composer/components/ShopgridComposerBar.jsx
+++ packages/composer/composer/components/ShopgridComposerBar.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import styles from './css/ShopgridComposerBar.css';
 
 import ComposerActionCreators from '../action-creators/ComposerActionCreators';
+import { IconArrowPopover } from '@bufferapp/components';
+import QuestionIcon from './QuestionIcon';
 
 class ShopgridComposerBar extends React.Component {
   constructor(props) {
@@ -13,6 +15,7 @@ class ShopgridComposerBar extends React.Component {
 
     this.onChange = this.onChange.bind(this);
     this.shouldShowShopgridBar = this.shouldShowShopgridBar.bind(this);
+    this.openHelp = this.openHelp.bind(this);
   }
 
   onChange(event) {
@@ -33,19 +36,34 @@ class ShopgridComposerBar extends React.Component {
     return hasInstagramSelected && isInstagram && hasShopgridFlip && isBusinessUser;
   }
 
+  openHelp(e) {
+    e.preventDefault();
+    window.open('https://faq.buffer.com/article/1164-publish-instagram-shop-grid', '_blank');
+  }
+
   render () {
     return (
       this.shouldShowShopgridBar() &&
       <div className={styles.shopgridComposerBar}>
         <div className={styles.shopgridFieldContainer}>
-          <span className={styles.shopgridFieldLabel}>Link </span>
+          <span className={styles.shopgridFieldLabel}>
+            Link
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.questionIcon}
+              onClick={this.openHelp}
+            >
+              <QuestionIcon />
+            </a>
+          </span>
           <span className={styles.shopgridAutocompleteContainer}>
             <input
               className={styles.shopgridFieldInput}
               onChange={this.onChange}
               onBlur={this.onChange}
               name="shopgridLink"
-              placeholder="Website or Product URL"
+              placeholder="URL for Shop Grid"
               value={this.state.shopgridLink}
             />
           </span>
