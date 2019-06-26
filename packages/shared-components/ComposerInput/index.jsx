0f6f9e5549e5214b817cd28fb77434b342e26f5d
anafilipadealmeida <anafilipadealmeida@gmail.com>
2019-06-27 00:23:21 +0100

    Add ComposerInput shared component

---
+++ packages/shared-components/ComposerInput/index.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CameraIcon from '@bufferapp/ui/Icon/Icons/Camera';
+
+const composerInputStyle = {
+  alignItems: 'center',
+  backgroundColor: '#ffffff',
+  border: '1px solid #B8B8B8',
+  borderRadius: '4px',
+  boxShadow: '0 1px 4px rgba(0,0,0,.16)',
+  color: '#999999',
+  cursor: 'pointer',
+  display: 'flex',
+  fontFamily: 'Roboto',
+  fontSize: '14px',
+  height: '48px',
+  outline: 'none',
+  paddingLeft: '16px',
+  paddingRight: '16px',
+  width: '100%',
+};
+
+const composerInputIcoCameraStyle = {
+  width: '16px',
+  height: '16px',
+  marginLeft: 'auto',
+};
+
+const ComposerInput = ({
+  onPlaceholderClick,
+  Placeholder,
+  isDisabled,
+}) =>
+  <button
+    style={composerInputStyle}
+    onClick={onPlaceholderClick}
+    disabled={isDisabled}
+  >
+    {Placeholder}
+    <div style={composerInputIcoCameraStyle}>
+      <CameraIcon />
+    </div>
+  </button>;
+
+
+ComposerInput.propTypes = {
+  onPlaceholderClick: PropTypes.func,
+  Placeholder: PropTypes.string,
+  isDisabled: PropTypes.bool,
+};
+
+ComposerInput.defaultProps = {
+  isDisabled: false,
+};
+
+export default ComposerInput;
---
+++ packages/shared-components/ComposerInput/story.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  storiesOf,
+  action,
+} from '@storybook/react';
+import { checkA11y } from 'storybook-addon-a11y';
+import ComposerInput from './index';
+
+storiesOf('ComposerInput', module)
+  .addDecorator(checkA11y)
+  .add('default', () => (
+    <ComposerInput
+      onPlaceholderClick={action('placeholder-click')}
+      Placeholder={'What would you like to share?'}
+    />
+  ))
+  .add('disabled', () => (
+    <ComposerInput
+      onPlaceholderClick={action('placeholder-click')}
+      Placeholder={'What would you like to share?'}
+      isDisabled
+    />
+  ));
