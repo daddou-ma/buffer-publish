dd15da4f765933d17b69732a2a53bda3339fa9fe
Ana Almeida <anafilipadealmeida@gmail.com>
2019-07-15 13:13:05 +0100

    Add index.js test

--- packages/tabs/index.test.js
+++ packages/tabs/index.test.js
@@ -1,6 +1,77 @@
-describe('example', () => {
-  it('should pass', () => {
-    expect(2 + 2)
-      .toBe(4);
+import React from 'react';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+
+import TabNavigation, {
+  reducer,
+  actions,
+  actionTypes,
+  middleware,
+} from './index';
+
+const storeFake = state => ({
+  default: () => {},
+  subscribe: () => {},
+  dispatch: () => {},
+  getState: () => ({ ...state }),
+});
+
+describe('TabNavigation', () => {
+  const store = storeFake({
+    profileSidebar: {
+      selectedProfile: {
+        business: true,
+        isManager: true,
+        type: 'linkedin',
+      },
+      isLockedProfile: false,
+    },
+    appSidebar: {
+      user: {
+        canStartProTrial: true,
+        trial: {
+          onTrial: false,
+        },
+      },
+    },
+    generalSettings: {
+      isInstagramProfile: false,
+    },
+    environment: {
+      environment: 'production',
+    },
+    productFeatures: {
+      planName: 'pro',
+    },
+  });
+
+  it('should render', () => {
+    const wrapper = mount(
+      <Provider store={store}>
+        <TabNavigation />
+      </Provider>,
+    );
+    expect(wrapper.find(TabNavigation).length)
+      .toBe(1);
+  });
+
+  it('should export reducer', () => {
+    expect(reducer)
+      .toBeDefined();
+  });
+
+  it('should export actions', () => {
+    expect(actions)
+      .toBeDefined();
+  });
+
+  it('should export actionTypes', () => {
+    expect(actionTypes)
+      .toBeDefined();
+  });
+
+  it('should export middleware', () => {
+    expect(middleware)
+      .toBeDefined();
   });
 });
