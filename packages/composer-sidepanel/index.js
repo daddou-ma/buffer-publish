d3c4a2c6f51a863064b299af6399e4509979dcd2
Maya Uribe <mariale.uribe@gmail.com>
2019-07-18 14:47:15 +0200

    Removing unnecessary redux connect.

--- packages/composer-sidepanel/index.js
+++ packages/composer-sidepanel/index.js
@@ -1,8 +1,3 @@
-import { connect } from 'react-redux';
 import ComposerSidepanel from './components/ComposerSidepanel';
 
-export default connect(
-  state => ({
-    userData: state.appSidebar.user,
-  }),
-)(ComposerSidepanel);
+export default ComposerSidepanel;
