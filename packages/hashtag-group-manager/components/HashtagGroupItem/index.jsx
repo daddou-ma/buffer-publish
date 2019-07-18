e6dca17fce464b5895b63d4d305f9ea7685967dc
Ana Almeida <anafilipadealmeida@gmail.com>
2019-07-18 22:27:48 +0100

    Add HashtagGroupItem structure

--- packages/hashtag-group-manager/components/HashtagGroupItem/index.jsx
+++ packages/hashtag-group-manager/components/HashtagGroupItem/index.jsx
@@ -1,13 +1,127 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Text, Button } from '@bufferapp/ui';
+import Select from '@bufferapp/ui/Select';
+import MoreIcon from '@bufferapp/ui/Icon/Icons/More';
+import TrashIcon from '@bufferapp/ui/Icon/Icons/Trash';
+import { grayDarker, gray } from '@bufferapp/ui/style/colors';
+import { Divider } from '@bufferapp/components';
 
-const HashtagGroupItem = () => {
+const getHashtagsStyles = hover => ({
+  fontSize: '12px',
+  lineHeight: '21px',
+  color: hover ? grayDarker : gray,
+  textOverflow: 'ellipsis',
+  whiteSpace: 'nowrap',
+  overflow: 'hidden',
+});
 
+const nameWrapperStyles = {
+  paddingRight: '8px',
+  flex: 1,
+  textOverflow: 'ellipsis',
+  whiteSpace: 'nowrap',
+  overflow: 'hidden',
 };
 
-HashtagGroupItem.propTypes = {
+const cardStyle = {
+  padding: '16px',
+  display: 'flex',
+  alignItems: 'center',
+};
 
+const cardHoverStyle = {
+  backgroundColor: 'rgba(107, 129, 255, 0.15)',
+  transition: 'all 0.1s ease-out',
+};
+
+const buttonWrapperStyle = {
+  display: 'flex',
+  marginLeft: '16px',
+  alignItems: 'center',
+};
+
+const getItemWrapperStyle = ({ state }) => {
+  let style = cardStyle;
+  if (state.hover) {
+    style = { ...style, ...cardHoverStyle };
+  }
+  return style;
+};
+
+class HashtagGroupItem extends Component {
+  state = {
+    hover: false,
+  }
+
+  render() {
+    const {
+      hashtagGroupItem,
+      onInsertClick,
+      onDeleteClick,
+    } = this.props;
+
+    return (
+      <Fragment>
+        <div
+          onMouseEnter={() => this.setState({ hover: true })}
+          onMouseLeave={() => this.setState({ hover: false })}
+          style={getItemWrapperStyle({ state: this.state })}
+        >
+          <div style={{ flex: '1', overflow: 'hidden' }}>
+            <div style={{ display: 'flex' }}>
+              <span style={nameWrapperStyles}>
+                <Text type="label">Woofers-Booping Snoots & Doing Friday</Text>
+              </span>
+              <Text type="label" color={gray}>17</Text>
+            </div>
+            <div style={getHashtagsStyles(this.state.hover)}>
+              <Text>#Woofers #Bigwoofer #Woofpack</Text>
+            </div>
+          </div>
+          {this.state.hover &&
+            <div style={buttonWrapperStyle}>
+              <Button
+                type="primary"
+                size="small"
+                label="Insert"
+                onClick={onInsertClick}
+              />
+              <Select
+                onSelectClick={() => true}
+                label="Click me"
+                icon={<MoreIcon />}
+                type="primary"
+                items={[
+                  {
+                    id: 'deleteHashtagGroup',
+                    title: 'Delete Hashtag Group',
+                    icon: <TrashIcon color="gray" />,
+                    onItemClick: () => onDeleteClick(),
+                  },
+                ]}
+                hasIconOnly
+                xPosition="left"
+                yPosition="bottom"
+                hideSearch
+              />
+            </div>
+          }
+        </div>
+        <Divider
+          marginTop={0}
+          marginBottom={0}
+        />
+      </Fragment>
+    );
+  }
+
+}
+
+HashtagGroupItem.propTypes = {
+  onInsertClick: PropTypes.func.isRequired,
+  onDeleteClick: PropTypes.func.isRequired,
+  hashtagGroupItem: PropTypes.object.isRequired,
 };
 
 HashtagGroupItem.defaultProps = {
