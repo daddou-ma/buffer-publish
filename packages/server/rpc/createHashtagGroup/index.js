a03b4727ed1dbd9e0f474ebf05225f4904fee26e
Char Mays <charmaramays@gmail.com>
2019-07-22 16:25:47 -0700

    Added template rpc calls for delete and create

--- packages/server/rpc/createHashtagGroup/index.js
+++ packages/server/rpc/createHashtagGroup/index.js
@@ -2,16 +2,18 @@ const { method } = require('@bufferapp/buffer-rpc');
 const rp = require('request-promise');
 
 module.exports = method(
-  'hashtagGroups',
-  'fetch hashtag groups',
-  ({ organizationId }, { session }) =>
+  'createHashtagGroup',
+  'create hashtag group',
+  ({ organizationId, name, text }, { session }) =>
     rp({
-      uri: `${process.env.API_ADDR}/1/snippet_groups.json`,
-      method: 'GET',
+      uri: `${process.env.API_ADDR}/1/snippet_groups/create.json`,
+      method: 'POST',
       strictSSL: !(process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'),
       qs: {
         access_token: session.publish.accessToken,
         organization_id: organizationId,
+        name,
+        text,
       },
     })
     .then(result => JSON.parse(result)),
--- packages/server/rpc/deleteHashtagGroup/index.js
+++ packages/server/rpc/deleteHashtagGroup/index.js
@@ -2,16 +2,17 @@ const { method } = require('@bufferapp/buffer-rpc');
 const rp = require('request-promise');
 
 module.exports = method(
-  'hashtagGroups',
+  'deleteHashtagGroup',
   'fetch hashtag groups',
-  ({ organizationId }, { session }) =>
+  ({ organizationId, snippetId }, { session }) =>
     rp({
-      uri: `${process.env.API_ADDR}/1/snippet_groups.json`,
-      method: 'GET',
+      uri: `${process.env.API_ADDR}/1/snippet_groups/delete.json`,
+      method: 'POST',
       strictSSL: !(process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'),
       qs: {
         access_token: session.publish.accessToken,
         organization_id: organizationId,
+        snippet_id: snippetId,
       },
     })
     .then(result => JSON.parse(result)),
