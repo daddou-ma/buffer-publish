2abec19e5e7463245fba42db7df4fe67b835fd50
AL <amyleeannebennett@gmail.com>
2019-07-24 09:01:06 -0500

    Clean up globalAccount reducer

--- packages/globalAccount/reducer.js
+++ packages/globalAccount/reducer.js
@@ -1,22 +1,6 @@
 import { actionTypes as asyncDataFetchActionTypes } from '@bufferapp/async-data-fetch';
-//
-// export default (state = {}, action) => {
-//   switch (action.type) {
-//     case `globalAccount_${asyncDataFetchActionTypes.FETCH_SUCCESS}`:
-//       return {
-//         ...state,
-//         ...action.result,
-//       };
-//     default:
-//       return state;
-//   }
-// };
 
-
-export const initialState = {
-};
-
-export default (state = initialState, action) => {
+export default (state = {}, action) => {
   switch (action.type) {
     case `globalAccount_${asyncDataFetchActionTypes.FETCH_SUCCESS}`:
       return {
