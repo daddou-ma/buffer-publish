70eda477a1773b3d4f2fc498aa999cadc6dfffc4
AL <amyleeannebennett@gmail.com>
2019-07-24 09:15:43 -0500

    Add tracking utils test

---
+++ packages/composer/composer/tests/utils/TrackingUtils.test.js
@@ -0,0 +1,22 @@
+import { getComposerSource } from '../../utils/TrackingUtils';
+
+describe('Tracking Utils', () => {
+  describe('returns correct source', () => {
+    it('returns queue list', () => {
+      const source = getComposerSource({ tabId: 'queue', emptySlotMode: true });
+      expect(source).toEqual('queue_list');
+    });
+    it('returns queue', () => {
+      const source = getComposerSource({ tabId: 'queue', emptySlotMode: false });
+      expect(source).toEqual('queue');
+    });
+    it('returns share_again', () => {
+      const source = getComposerSource({ tabId: 'analytics' });
+      expect(source).toEqual('share_again');
+    });
+    it('returns source name if no match', () => {
+      const source = getComposerSource({ tabId: 'foobar' });
+      expect(source).toEqual('foobar');
+    });
+  });
+});
