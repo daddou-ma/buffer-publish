e58c0e2d73c1e61b677eb1d7cd49e5dc67bd2095
Maya Uribe <mariale.uribe@gmail.com>
2019-08-06 15:54:03 +0200

    Redirecting to FAQ so that users can delete their account.

--- packages/close-account/components/CloseAccount/index.jsx
+++ packages/close-account/components/CloseAccount/index.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Text, Button } from '@bufferapp/ui';
 import { Row } from '@bufferapp/publish-shared-components';
+import { Link } from '@bufferapp/components';
 import Modal from '../Modal';
 
 const CloseAccount = ({ showModal, onRequestOpenModal, onRequestCloseModal, onSubmit }) => (
   <Row>
     <div>
       <Text type="h3">Delete your Buffer account</Text>
-      <Text type="p">If you delete your account, you will lose all your posts.</Text>
+      <Text type="p">
+        <Link href="https://buffer.com/support" newTab>
+        Please contact support to delete your account
+        </Link>
+      </Text>
     </div>
-    <Button
-      type="text"
-      onClick={() => onRequestOpenModal()}
-      label="Delete Account"
-    />
+
     {showModal ? <Modal onRequestCloseModal={onRequestCloseModal} onSubmit={onSubmit} /> : null}
   </Row>
 );
