d69beaaa941e7badacb3c65138bb264a4119a99a
AL <amyleeannebennett@gmail.com>
2019-08-07 14:48:22 -0500

    Revert close account changes

--- packages/close-account/components/CloseAccount/index.jsx
+++ packages/close-account/components/CloseAccount/index.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Text, Button } from '@bufferapp/ui';
 import { Row } from '@bufferapp/publish-shared-components';
-import { Link } from '@bufferapp/components';
 import Modal from '../Modal';
 
 const CloseAccount = ({ showModal, onRequestOpenModal, onRequestCloseModal, onSubmit }) => (
   <Row>
     <div>
       <Text type="h3">Delete your Buffer account</Text>
-      <Text type="p">
-        Please contact <Link href="mailto:hello@buffer.com?subject=Request to delete my account" newTab>support</Link> to delete your account
-      </Text>
+      <Text type="p">If you delete your account, you will lose all your posts.</Text>
     </div>
-
+    <Button
+      type="text"
+      onClick={() => onRequestOpenModal()}
+      label="Delete Account"
+    />
     {showModal ? <Modal onRequestCloseModal={onRequestCloseModal} onSubmit={onSubmit} /> : null}
   </Row>
 );
