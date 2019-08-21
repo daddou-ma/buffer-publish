95f4400847e52e5068ea55390613124200fcef6e
Dace Willmott <dace@buffer.com>
2019-08-20 21:49:14 -0400

    Add component

---
+++ packages/upgrade-modal/components/CreditCardForm/form.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { CardElement, injectStripe } from 'react-stripe-elements';
+
+class CreditCardForm extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {}
+
+  componentDidUpdate() {}
+
+  componentWillUnmount() {}
+
+  onChange = () => {};
+
+  onSubmit = e => {
+    e.preventDefault();
+  };
+
+  render() {
+    return <CardElement />;
+  }
+}
+
+export default injectStripe(CreditCardForm);
