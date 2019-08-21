57560b2d96df14517b3e4297bdcdba4022edc21c
Eduardo Esclapes <eduardo.esclapes@gmail.com>
2019-08-21 15:50:43 +0200

    Remove extra console log and unnecesary return statement

--- packages/server/rpc/createSetupIntent/index.js
+++ packages/server/rpc/createSetupIntent/index.js
@@ -6,22 +6,19 @@ const strictSSL = !(process.env.NODE_ENV === 'development');
 module.exports = method(
   'createSetupIntent',
   'create a setup intent in Stripe',
-  ({ session }) => {
-    console.log('ARGS', arguments)
-    return rp({
-      uri: `${process.env.API_ADDR}/1/billing/create-setup-intent.json`,
-      method: 'POST',
-      strictSSL,
-      qs: {
-        access_token: session.publish.accessToken,
-      },
-    })
-      .then(result => JSON.parse(result))
-      .catch((err) => {
-        if (err.error) {
-          const { error } = JSON.parse(err.error);
-          throw createError({ message: error });
-        }
-      })
-    }
+  ({ session }) => rp({
+    uri: `${process.env.API_ADDR}/1/billing/create-setup-intent.json`,
+    method: 'POST',
+    strictSSL,
+    qs: {
+      access_token: session.publish.accessToken,
+    },
+  })
+    .then(result => JSON.parse(result))
+    .catch((err) => {
+      if (err.error) {
+        const { error } = JSON.parse(err.error);
+        throw createError({ message: error });
+      }
+    }),
 );
