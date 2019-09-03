d2ea637c1c5fddd5a423b31ba5a45d727f45a7bb
Ana Almeida <anafilipadealmeida@gmail.com>
2019-09-03 15:26:56 +0100

    Add Card shared-component

---
+++ packages/shared-components/Card/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { gray, white } from '@bufferapp/ui/style/colors';
+import { borderRadius } from '@bufferapp/ui/style/borders';
+
+const CardWrapper = styled.div`
+  display: flex;
+  background: ${white};
+  border: 1px solid ${gray};
+  box-sizing: border-box;
+  box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.16);
+  border-radius: ${borderRadius};
+`;
+
+const Card = ({
+  children,
+}) => (
+  <CardWrapper>
+    {children}
+  </CardWrapper>
+);
+
+
+Card.propTypes = {
+  children: PropTypes.node,
+};
+
+Card.defaultProps = {
+  children: null,
+};
+
+export default Card;
