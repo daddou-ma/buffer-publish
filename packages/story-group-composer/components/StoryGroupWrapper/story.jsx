0354c48d5a390b2ce487ad6220c11b7d270eed3a
AL <amyleeannebennett@gmail.com>
2019-09-04 13:50:07 -0500

    Update snapshots

--- packages/story-group-composer/components/StoryGroupWrapper/story.jsx
+++ packages/story-group-composer/components/StoryGroupWrapper/story.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { selectedProfile } from '@bufferapp/publish-profile-sidebar/mockData/profiles';
 import { checkA11y } from 'storybook-addon-a11y';
 import StoryGroupWrapper from './index';
 
 storiesOf('StoryGroup', module)
   .addDecorator(checkA11y)
   .add('story group wrapper', () => (
-    <StoryGroupWrapper />
+    <StoryGroupWrapper
+      selectedProfile={selectedProfile}
+    />
   ));
