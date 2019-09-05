78df52bddef4f9acfc13c786d219b73bbc89b3b3
AL <amyleeannebennett@gmail.com>
2019-09-05 11:07:18 -0500

    Update story

--- packages/story-group-composer/components/StoryGroupWrapper/story.jsx
+++ packages/story-group-composer/components/StoryGroupWrapper/story.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { selectedProfile } from '@bufferapp/publish-profile-sidebar/mockData/profiles';
+import translations from '@bufferapp/publish-i18n/translations/en-us.json';
 import { checkA11y } from 'storybook-addon-a11y';
 import StoryGroupWrapper from './index';
 
@@ -9,5 +10,11 @@ storiesOf('StoryGroup', module)
   .add('story group wrapper', () => (
     <StoryGroupWrapper
       selectedProfile={selectedProfile}
+      translations={translations['story-group-composer']}
+      saveNote={() => {}}
+      onDateTimeSlotPickerSubmit={() => {}}
+      uses24hTime
+      timezone="UTC"
+      weekStartsMonday={false}
     />
   ));
