274421e08400f04198b3625fdd14d6cf19b3a4e0
Ana Almeida <anafilipadealmeida@gmail.com>
2019-09-11 14:24:39 +0100

    Add basic components

--- packages/shared-components/LightboxCarousel/index.jsx
+++ packages/shared-components/LightboxCarousel/index.jsx
@@ -2,33 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Popover } from '@bufferapp/components';
-import { Text, Button } from '@bufferapp/ui';
-import { ArrowLeft, ArrowRight } from '@bufferapp/ui/Icon';
-import { white, transparent, red, grayLight, grayDarker, grayDark } from '@bufferapp/ui/style/colors';
-import { fontWeightBold } from '@bufferapp/ui/style/fonts';
+import { Button } from '@bufferapp/ui';
+import { ArrowLeft, ArrowRight, Cross } from '@bufferapp/ui/Icon';
+import { white, transparent } from '@bufferapp/ui/style/colors';
+import { borderRadius } from '@bufferapp/ui/style/borders';
 
 const ContentWrapper = styled.div`
   background-color: ${white};
   display: flex;
-  height: 592px;
-  width: 629px;
+  height: ${({ height }) => (height)}px;
+  width: ${({ width }) => (width)}px;
+  border-radius: ${borderRadius};
 `;
 
 const Modal = styled.div`
   display: flex;
-  height: 592px;
+  height: ${({ height }) => (height)}px;
 `;
 
 const ArrowColumn = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: ${red};
-  width: 48px;
-`;
-
-const RightColumn = styled.div`
-  background-color: ${red};
+  background-color: ${transparent};
   width: 48px;
 `;
 
@@ -40,18 +36,27 @@ const ArrowButton = styled(Button)`
   padding: 0;
 `;
 
+const CloseButton = styled(Cross)`
+  position: absolute;
+  right: 0;
+  cursor: pointer;
+  color: ${white};
+  right: 18px;
+`;
+
 const LightboxCarousel = ({
   children,
   height,
   width,
   onOverlayClick,
+  onCloseClick,
 }) => (
   <Popover
     width="100%"
     top="5rem"
     onOverlayClick={onOverlayClick}
   >
-    <Modal>
+    <Modal height={height}>
       <ArrowColumn>
         <ArrowButton
           type="secondary"
@@ -62,8 +67,9 @@ const LightboxCarousel = ({
         />
       </ArrowColumn>
       <ContentWrapper height={height} width={width}>
-        <p></p>
+        {children}
       </ContentWrapper>
+      <CloseButton size="large" onClick={onCloseClick} />
       <ArrowColumn>
         <ArrowButton
           type="secondary"
@@ -77,13 +83,20 @@ const LightboxCarousel = ({
   </Popover>
 );
 
-
 LightboxCarousel.propTypes = {
   children: PropTypes.node.isRequired,
   onOverlayClick: PropTypes.func,
+  onCloseClick: PropTypes.func,
+  height: PropTypes.number,
+  width: PropTypes.number,
+
 };
 
 LightboxCarousel.defaultProps = {
+  onOverlayClick: null,
+  onCloseClick: null,
+  height: 592,
+  width: 629,
 };
 
 export default LightboxCarousel;
