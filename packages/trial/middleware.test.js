24cfa8831e3330327219fc8b220f07ec0d3b6ffd
Karel McGrail <karel@buffer.com>
2019-09-18 14:28:45 -0700

    skipping draft test + fixing trial middleware test

--- packages/trial/middleware.test.js
+++ packages/trial/middleware.test.js
@@ -1,6 +1,6 @@
-import { actionTypes as modalsActionTypes } from '@bufferapp/publish-modals';
+import { actionTypes as modalsActionTypes } from '@bufferapp/publish-modals/reducer';
 import { trackAction } from '@bufferapp/publish-data-tracking';
-import { actionTypes as notificationActionTypes } from '@bufferapp/notifications';
+import { actionTypes as notificationActionTypes } from '@bufferapp/notifications/lib/reducer';
 import {
   actions as dataFetchActions,
   actionTypes as dataFetchActionTypes,
