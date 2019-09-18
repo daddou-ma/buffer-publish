5c2102395fdeb74206978e43d9f9442387612935
Karel McGrail <karel@buffer.com>
2019-09-18 16:41:41 -0700

    removing unnecessary polyfill in init storybook code

--- snapshot.test.js
+++ snapshot.test.js
@@ -1,33 +1,4 @@
 // use story.js files as snapshots
-import initStoryshots, { snapshotWithOptions } from '@storybook/addon-storyshots';
+import initStoryshots from '@storybook/addon-storyshots';
 
-const skipSupplyingRefs = [
-  'CloseComposerConfirmationModal',
-  'TwoFactorAuth',
-  'SwitchPlanModal',
-  'Sidebar',
-  'ProfileSidebar',
-  'ProfileSearch',
-  'ProfilePage',
-  'HashtagGroup',
-  'App',
-  'AppShell',
-];
-
-initStoryshots({
-  suit: 'Snapshots',
-  configPath: '.storybookStoryshot/',
-  test: snapshotWithOptions(story => (skipSupplyingRefs.includes(story.kind) ? {} : ({
-    createNodeMock: (element) => {
-      const { Elements, StripeProvider } = require('react-stripe-elements');
-
-      if (element.type === Elements) {
-        return null;
-      }
-      if (element.type === StripeProvider) {
-        return null;
-      }
-      return element;
-    },
-  }))),
-});
+initStoryshots({ suit: 'Snapshots', configPath: '.storybookStoryshot/' });
