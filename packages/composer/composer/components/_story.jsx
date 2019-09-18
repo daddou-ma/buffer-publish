c8bcec0e423bc9c6aa2fc6455db410d4fab01dec
Karel McGrail <karel@buffer.com>
2019-09-17 18:14:29 -0700

    removing pusher mock as theres already a stub for this

--- packages/composer/composer/components/_story.jsx
+++ packages/composer/composer/components/_story.jsx
@@ -3,11 +3,6 @@ import { storiesOf } from '@storybook/react';
 import Composer from './App';
 import useCases from '../.storybook/mock-initial-props';
 
-jest.mock('pusher-js', () => {
-  const Pusher = require('pusher-js-mock');
-  return Pusher.PusherMock;
-});
-
 const stories = {
   create: 'Dashboard (create new)',
   edit: 'Dashboard (edit custom-time update)',
