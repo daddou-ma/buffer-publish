3521356131bbb0ed0ce19c6fc2f6ab104189414d
Ana Almeida <anafilipadealmeida@gmail.com>
2019-09-24 12:03:03 +0100

    Add segment constants

--- packages/constants/index.js
+++ packages/constants/index.js
@@ -38,6 +38,8 @@ const PLANS_PREMIUM_DOWNGRADE = 'publish-plans-switchPlansModal-premiumDowngrade
 const PLANS_SMALL_UPGRADE = 'publish-plans-switchPlansModal-smallUpgrade-1';
 const PLANS_SMALL_DOWNGRADE = 'publish-plans-switchPlansModal-smallDowngrade-1';
 const PLANS_OPEN_MODAL = 'publish-plans-subscribeButton-openSwithPlansModal-1';
+const STORIES_PREVIEW_COMPOSER = 'publish-stories-composer-preview-1';
+const STORIES_PREVIEW_QUEUE = 'publish-stories-queue-preview-1';
 
 const UploadTypes = keyMirror({
   LINK_THUMBNAIL: null,
@@ -109,6 +111,8 @@ module.exports = {
     PLANS_SMALL_UPGRADE,
     PLANS_SMALL_DOWNGRADE,
     PLANS_OPEN_MODAL,
+    STORIES_PREVIEW_COMPOSER,
+    STORIES_PREVIEW_QUEUE,
   },
   MediaTypes,
   UploadTypes,
