6052ca0a5c14e97b484cce82f33ee75c2ac6d243
Karel McGrail <karel@buffer.com>
2019-09-24 16:47:21 +0100

    Modifying storybook config for greater consistency

--- .storybookStoryshot/config.js
+++ .storybookStoryshot/config.js
@@ -2,9 +2,11 @@ const babelConfig = require('../babel.config.js');
 require('@babel/register')(babelConfig);
 require('core-js/stable');
 require('regenerator-runtime/runtime');
-const { configure, storiesOf } = require('@storybook/react');
+const { configure } = require('@storybook/react');
 
-// Add all story.jsx files inside components directories except for the ones in node_modules
-const req = require.context('../packages', true, /^((?![\\/]node_modules[\\/]).)*components\/.*\/*story\.jsx$/)
-const loadStories = () => req.keys().forEach(req)
-configure(loadStories, module)
+function loadStories() {
+  // Add all story.jsx files inside components directories except for the ones in node_modules
+  const req = require.context('../packages', true, /^((?![\\/]node_modules[\\/]).)*components\/.*\/*story\.jsx$/)
+  req.keys().forEach(filename => req(filename));
+}
+configure(loadStories, module);
