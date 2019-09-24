047cd540a4fbd9910ee1186539617b82959ff53d
Karel McGrail <karel@buffer.com>
2019-09-24 16:29:14 +0100

    Updating config for tests in composer

--- packages/composer/composer/.storybook/config.js
+++ packages/composer/composer/.storybook/config.js
@@ -1,8 +1,10 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-import { configure } from '@storybook/react';
+const babelConfig = require('../../../../babel.config.js');
+require('@babel/register')(babelConfig);
+require('core-js/stable');
+require('regenerator-runtime/runtime');
+const { configure } = require('@storybook/react');
 
-const req = require.context('../components', true, /story\.jsx$/);
+const req = require.context('../components', true, /^((?![\\/]node_modules[\\/]).)*components\/.*\/*story\.jsx$/);
 const loadStories = () => req.keys().forEach(req);
 
 configure(loadStories, module);
