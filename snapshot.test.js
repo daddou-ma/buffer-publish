dcdfa4dabd0bb9b63dc806d14aa8d81ce80cc2bf
Karel McGrail <karel@buffer.com>
2019-09-24 16:36:10 +0100

    Adding support for import statements with storybook tests

--- snapshot.test.js
+++ snapshot.test.js
@@ -1,4 +1,10 @@
-// use story.js files as snapshots
+const babelConfig = require('./babel.config.js');
+require('@babel/register')(babelConfig);
+require('core-js/stable');
+require('regenerator-runtime/runtime');
 import initStoryshots from '@storybook/addon-storyshots';
 
-initStoryshots({ suit: 'Snapshots', configPath: '.storybookStoryshot/' });
+initStoryshots({
+  suit: 'Snapshots',
+  configPath: '.storybookStoryshot/',
+});
