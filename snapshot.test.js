6a4b2b726dbe2a59ed18916dc4139de66204e572
Karel McGrail <karel@buffer.com>
2019-09-24 17:13:54 +0100

    Updating storybook config

--- snapshot.test.js
+++ snapshot.test.js
@@ -2,7 +2,7 @@ const babelConfig = require('./babel.config.js');
 require('@babel/register')(babelConfig);
 require('core-js/stable');
 require('regenerator-runtime/runtime');
-import initStoryshots from '@storybook/addon-storyshots';
+const initStoryshots = require('@storybook/addon-storyshots').default;
 
 initStoryshots({
   suit: 'Snapshots',
