38c21d3d2dc3853f0d7f486eb1a4a5c037c7b3ab
AL <amyleeannebennett@gmail.com>
2019-09-27 11:28:23 -0500

    Add test for sg updated tracking

--- packages/story-group-composer/middleware.test.js
+++ packages/story-group-composer/middleware.test.js
@@ -17,23 +17,23 @@ describe('middleware', () => {
         },
       },
     }));
-
+    const storyGroup = {
+      storyDetails: {
+        stories: [],
+        id: '123',
+        profileId: '123',
+        scheduled_at: '29838471',
+      },
+    };
     it('should send tracking data when story group is created', () => {
-      const storyGroup = {
-        storyDetails: {
-          stories: [],
-          id: '123',
-          profileId: '123',
-          scheduled_at: '29838471',
-        },
-      };
       const action = {
         type: `createStoryGroup_${dataFetchActionTypes.FETCH_SUCCESS}`,
         result: { storyGroup },
       };
-      const expectedObj = getCreateSGTrackingData({
+      const expectedObj = getSGTrackingData({
         storyGroup,
         channel: getState().profileSidebar.selectedProfile,
+        cta: SEGMENT_NAMES.STORIES_CREATE_STORY_GROUP,
       });
 
       middleware({ dispatch, getState })(next)(action);
@@ -43,6 +43,25 @@ describe('middleware', () => {
 
       expect(analyticsActions.trackEvent)
         .toBeCalledWith('Story Created', expectedObj);
+    });
+    it('should send tracking data when story group is updated', () => {
+      const action = {
+        type: `updateStoryGroup_${dataFetchActionTypes.FETCH_SUCCESS}`,
+        result: { storyGroup },
+      };
+      const expectedObj = getSGTrackingData({
+        storyGroup,
+        channel: getState().profileSidebar.selectedProfile,
+        cta: SEGMENT_NAMES.STORIES_UPDATE_STORY_GROUP,
+      });
+
+      middleware({ dispatch, getState })(next)(action);
+
+      expect(next)
+        .toBeCalledWith(action);
+
+      expect(analyticsActions.trackEvent)
+        .toBeCalledWith('Story Updated', expectedObj);
     });
   });
 });
