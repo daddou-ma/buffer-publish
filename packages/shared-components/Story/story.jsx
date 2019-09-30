7a2833163466744e91288ad33de34d5c468f639a
AL <amyleeannebennett@gmail.com>
2019-09-30 12:08:51 -0500

    Small storybook refactor

--- packages/shared-components/Story/story.jsx
+++ packages/shared-components/Story/story.jsx
@@ -14,11 +14,8 @@ const storyDetails = {
 };
 
 const storyDetailsWithError = {
-  creatorName: 'Ash',
-  avatarUrl: 'https://buffer-uploads.s3.amazonaws.com/510521020a19000b6a00001e/a476fed03b1de4e06563d6063d7d3ee0.jpg',
-  createdAt: 'March 2nd at 12:45pm (GMT)',
-  storyAction: 'You will receive a reminder today at 2:55 PM (BST) when it is time to post',
-  error: 'There was an error sharing this story. Please try again',
+  ...storyDetails,
+  error: 'There was an error sharing this story. Please try again!',
 };
 
 storiesOf('Cards|Stories/Story', module)
