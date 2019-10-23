c2ff21811f9bcf1575f88c7baa88840f4d7443ab
Hamish Macpherson <hamstu@gmail.com>
2019-10-23 14:23:25 -0700

    use absolute package import

--- packages/composer/composer/components/InstagramUserTags.jsx
+++ packages/composer/composer/components/InstagramUserTags.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import UserTags from '@bufferapp/publish-composer-user-tags';
 
 import Modal from '../__legacy-buffer-web-shared-components__/modal/modal';
 import ModalActionCreators from '../__legacy-buffer-web-shared-components__/modal/actionCreators';
 import ComposerActionCreators from '../action-creators/ComposerActionCreators';
-import UserTags from '../../../composer-user-tags';
 
 const InstagramUserTags = ({ media, draftId, userTags = [] }) => {
   const saveGlobalTags = tags => {
