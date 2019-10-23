ac0920c3ffd00ccef42d98822ed5d36a424d21dc
Hamish Macpherson <hamstu@gmail.com>
2019-10-23 10:40:53 -0700

    move composer user tagging into it's own package

---
+++ packages/composer-user-tags/components/index.jsx
@@ -0,0 +1,212 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Text, Button } from '@bufferapp/ui';
+import { ArrowLeft, Cross, Person } from '@bufferapp/ui/Icon';
+import Input from '@bufferapp/ui/Input';
+
+// import ModalActionCreators from '../__legacy-buffer-web-shared-components__/modal/actionCreators';
+// import ComposerActionCreators from '../action-creators/ComposerActionCreators';
+
+import {
+  UserName,
+  ButtonWrapper,
+  PersonIcon,
+  PlainText,
+  Modal,
+  ModalInner,
+  RightHeader,
+  Image,
+  RightContent,
+} from './style';
+
+const calculateTagStyles = ({ tag, showTags }) => ({
+  position: 'absolute',
+  left: `${tag.clientX}px`,
+  top: `${tag.clientY}px`,
+  color: 'white',
+  backgroundColor: 'black',
+  borderRadius: '7%',
+  padding: '2px 5px',
+  opacity: '0.7',
+  display: showTags ? 'block' : 'none',
+});
+
+const UserTags = ({ media, userTags = [] }) => {
+  const initialCoordinateState = {
+    x: null,
+    y: null,
+    clientX: null,
+    clientY: null,
+  };
+  const [coordinates, setCoordinates] = useState(initialCoordinateState);
+  const [tags, setTags] = useState(userTags);
+  const [showTags, setShowTags] = useState(true);
+  const [inputValue, setInputValue] = useState('');
+
+  const saveTag = () => {
+    const { x, y, clientX, clientY } = coordinates;
+    const userTag = {
+      username: inputValue,
+      x,
+      y,
+      clientX,
+      clientY,
+    };
+    setTags([...tags, userTag]);
+    setInputValue('');
+    setCoordinates(initialCoordinateState);
+  };
+
+  const saveGlobalTags = () => {
+    // ComposerActionCreators.updateDraftUserTags(draftId, tags);
+    // ModalActionCreators.closeModal();
+    // console.log('save tags');
+    // TODO: use passed in prop to send up
+  };
+
+  const removeTag = removedUserTag => {
+    const newTagArray = tags.filter(tag => tag !== removedUserTag);
+    setTags(newTagArray);
+  };
+
+  const onImageClick = e => {
+    const rect = e.target.getBoundingClientRect();
+    const clientX = e.clientX - rect.left; // x position within the element.
+    const clientY = e.clientY - rect.top; // y position within the element.
+    // final_width = max_height * start_width / start_height
+    let { width, height } = media;
+    if (height > 500) {
+      width = (500 * width) / height;
+      height = 500;
+    }
+    const x = clientX / width;
+    const y = clientY / height;
+    setCoordinates({
+      x: x.toFixed(2),
+      y: y.toFixed(2),
+      clientX,
+      clientY,
+    });
+
+    e.preventDefault();
+  };
+
+  const onTogglePersonIcon = () => setShowTags(!showTags);
+
+  const TagImageLabel = ({ tag, index }) => (
+    <div key={index} style={calculateTagStyles({ tag, showTags })}>
+      {tag.username}
+    </div>
+  );
+  TagImageLabel.propTypes = {
+    tag: PropTypes.shape({
+      username: PropTypes.string,
+      x: PropTypes.number,
+      y: PropTypes.number,
+    }).isRequired,
+    index: PropTypes.number.isRequired,
+  };
+
+  // TO-DO: refactor to be more a11y friendly
+  const TagListItem = ({ tag, index }) => (
+    <UserName key={index}>
+      <button
+        type="button"
+        onClick={() => {
+          removeTag(tag);
+        }}
+        tabIndex={0}
+      >
+        <Cross size="medium" />
+      </button>
+      {tag.username}
+    </UserName>
+  );
+  TagListItem.propTypes = {
+    tag: PropTypes.shape({
+      username: PropTypes.string,
+      x: PropTypes.number,
+      y: PropTypes.number,
+    }).isRequired,
+    index: PropTypes.number.isRequired,
+  };
+
+  return (
+    <Modal>
+      <ModalInner>
+        <Image alt="" src={media.url} onClick={onImageClick} />
+        <PersonIcon onClick={onTogglePersonIcon}>
+          <Person size="large" />
+        </PersonIcon>
+        {tags && (
+          <div>
+            {tags.map((tag, index) => (
+              <TagImageLabel tag={tag} index={index} />
+            ))}
+          </div>
+        )}
+        <RightContent>
+          <RightHeader>
+            <span>
+              <ArrowLeft size="large" />
+            </span>
+            <Text type="h3">Tag a user</Text>
+          </RightHeader>
+          <Text>Click on a spot in the photo to tag</Text>
+          <div style={{ display: 'flex' }}>
+            <div>@</div>
+            <Input
+              type="input"
+              onChange={e => {
+                setInputValue(e.target.value);
+              }}
+              placeholder="username"
+              value={inputValue}
+              name="tagInput"
+            />
+            <ButtonWrapper>
+              <Button
+                type="primary"
+                size="small"
+                onClick={saveTag}
+                label="Save tag"
+                disabled={!coordinates.y}
+              />
+            </ButtonWrapper>
+          </div>
+          {tags && (
+            <div>
+              {tags.map((tag, index) => (
+                <TagListItem tag={tag} index={index} />
+              ))}
+            </div>
+          )}
+          <PlainText>
+            Note: The API only allows public users to be tagged and prevents
+            autocomplete in usernames. // Move to translations
+          </PlainText>
+          <Button onClick={saveGlobalTags} label="Save and Close" />
+        </RightContent>
+      </ModalInner>
+    </Modal>
+  );
+};
+
+UserTags.propTypes = {
+  media: PropTypes.shape({
+    url: PropTypes.string,
+    width: PropTypes.number,
+    height: PropTypes.number,
+  }).isRequired,
+  userTags: PropTypes.arrayOf({
+    userName: PropTypes.string,
+    x: PropTypes.number,
+    y: PropTypes.number,
+  }),
+};
+
+UserTags.defaultProps = {
+  userTags: [],
+};
+
+export default UserTags;
