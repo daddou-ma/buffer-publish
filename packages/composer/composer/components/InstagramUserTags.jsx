f91a9009fe187763ef2e50d1b641644c111fa966
Hamish Macpherson <hamstu@gmail.com>
2019-10-24 15:44:18 -0700

    fix modal pos. and disallow close on overlay click

--- packages/composer/composer/components/InstagramUserTags.jsx
+++ packages/composer/composer/components/InstagramUserTags.jsx
@@ -5,14 +5,18 @@ import UserTags from '@bufferapp/publish-composer-user-tags';
 import Modal from '../__legacy-buffer-web-shared-components__/modal/modal';
 import ModalActionCreators from '../__legacy-buffer-web-shared-components__/modal/actionCreators';
 import ComposerActionCreators from '../action-creators/ComposerActionCreators';
+import style from './css/InstagramUserTags.css';
 
 const InstagramUserTags = ({ media, draftId, userTags = [] }) => {
+  const modalClassNames = {
+    modal: style.modal,
+  };
   const saveGlobalTags = tags => {
     ComposerActionCreators.updateDraftUserTags(draftId, tags);
     ModalActionCreators.closeModal();
   };
   return (
-    <Modal hideCloseButton>
+    <Modal classNames={modalClassNames} hideCloseButton>
       <UserTags
         media={media}
         userTags={userTags}
