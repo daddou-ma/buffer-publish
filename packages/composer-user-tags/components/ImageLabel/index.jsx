f4817be2a86f1c5e7a2bd10b6698d5ac697e0a92
AL <amyleeannebennett@gmail.com>
2019-10-28 14:48:46 -0500

    Refactor image label styles into styled component

--- packages/composer-user-tags/components/ImageLabel/index.jsx
+++ packages/composer-user-tags/components/ImageLabel/index.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-const calculateTagStyles = ({ tag, showTags }) => ({
-  position: 'absolute',
-  left: `${tag.clientX}px`,
-  top: `${tag.clientY}px`,
-  color: 'white',
-  backgroundColor: 'black',
-  borderRadius: '7%',
-  padding: '2px 5px',
-  opacity: '0.7',
-  display: showTags ? 'block' : 'none',
-});
+const StyledImageLabel = styled.div`
+  position: absolute;
+  left: ${({ tag }) => tag.clientX}px;
+  top: ${({ tag }) => tag.clientY}px;
+  color: white;
+  background-color: black;
+  border-radius: 7%;
+  padding: 2px 5px;
+  opacity: 0.7;
+  display: ${({ showTags }) => (showTags ? 'block' : 'none')};
+`;
 
 const ImageLabel = ({ tag, showTags }) => (
-  <div style={calculateTagStyles({ tag, showTags })}>{tag.username}</div>
+  <StyledImageLabel tag={tag} showTags={showTags}>
+    {tag.username}
+  </StyledImageLabel>
 );
 
 ImageLabel.propTypes = {
