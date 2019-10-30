d96033443205763120023bc743705f060e86b681
AL <amyleeannebennett@gmail.com>
2019-10-29 19:52:06 -0500

    Update tests

--- packages/composer-user-tags/utils/Tags.test.js
+++ packages/composer-user-tags/utils/Tags.test.js
@@ -1,33 +1,24 @@
-import moment from 'moment-timezone';
 import { getClientXY, removeClientXY } from './Tags';
 
 describe('Tags Utils', () => {
+  const xyObj = { x: 0.5, y: 0.2 };
   describe('getClientXY', () => {
-    const xyObj = { x: 0.5, y: 0.2 };
-    it('calculates clientX & clientY for each item in array', () => {
-      const arrToSend = [...xyObj];
-      const format = getClientXY([...objToSend]);
-      expect(format).toEqual([{ ...objToSend, clientX: }]);
-    });
-    it('returns readable date with BST timezone and 24h time', () => {
-      const objToSend = {
-        uses24hTime: true,
-        scheduledAt: 1569261207,
-        timezone: 'Europe/London',
-      };
-      const format = getReadableDateFormat(objToSend);
-      expect(format).toEqual('Sep 23, 18:53');
+    it('adds clientX & clientY for each item in array', () => {
+      const format = getClientXY([...xyObj]);
+      expect(format).toEqual([{ ...xyObj, clientX: 50, y: 20 }]);
     });
   });
   describe('removeClientXY', () => {
     it('removes clientX & clientY props from array items', () => {
-      const arrToSend = [{
-        ...xyObj,
-        clientX: 282,
-        clientY: 302.2,
-      }];
+      const arrToSend = [
+        {
+          ...xyObj,
+          clientX: 50,
+          clientY: 20,
+        },
+      ];
       const format = removeClientXY(arrToSend);
-      expect(arrToSend).toEqual([...xyObj]);
+      expect(format).toEqual([...xyObj]);
     });
   });
 });
