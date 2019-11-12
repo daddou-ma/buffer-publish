a21e3d0f592a5559847cf06ec9ec539f0ec3cdf2
Char Mays <charmaramays@gmail.com>
2019-11-12 11:49:07 -0800

    Add RPCs for user tags

---
+++ packages/server/rpc/removeUserTag/index.js
@@ -0,0 +1,24 @@
+const { method, createError } = require('@bufferapp/buffer-rpc');
+const rp = require('request-promise');
+
+module.exports = method(
+  'removeUserTag',
+  'remove tag from user',
+  ({ tag }, { session }) =>
+    rp({
+      uri: `${process.env.API_ADDR}/1/user/remove_tag.json`,
+      method: 'POST',
+      strictSSL: !(process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'),
+      qs: {
+        access_token: session.publish.accessToken,
+        tag,
+      },
+    })
+    .then(result => JSON.parse(result))
+    .catch((err) => {
+      if (err.error) {
+        const error = JSON.parse(err.error);
+        throw createError({ message: error.message });
+      }
+    }),
+);
