89a61de6d09e39ebcea1d6c3a42df395cf16fa0a
Ana Almeida <anafilipadealmeida@gmail.com>
2019-11-21 13:51:54 +0000

    Update middleware test

--- packages/sent/middleware.test.js
+++ packages/sent/middleware.test.js
@@ -1,34 +1,29 @@
-import { actionTypes as profileActionTypes } from '@bufferapp/publish-profile-sidebar/reducer';
-import {
-  actions as dataFetchActions,
-} from '@bufferapp/async-data-fetch';
+import { actions as dataFetchActions } from '@bufferapp/async-data-fetch';
 import middleware from './middleware';
+import { actionTypes } from './reducer';
 
 describe('middleware', () => {
   const next = jest.fn();
   const dispatch = jest.fn();
   it('should export middleware', () => {
-    expect(middleware)
-      .toBeDefined();
+    expect(middleware).toBeDefined();
   });
 
   it('should fetch sentPosts', () => {
     const action = {
-      type: profileActionTypes.SELECT_PROFILE,
-      profile: {
-        id: 'id1',
-      },
+      type: actionTypes.FETCH_SENT_POSTS,
+      profileId: 'id1',
     };
     middleware({ dispatch })(next)(action);
-    expect(next)
-    .toBeCalledWith(action);
-    expect(dispatch)
-    .toBeCalledWith(dataFetchActions.fetch({
-      name: 'sentPosts',
-      args: {
-        profileId: action.profile.id,
-        isFetchingMore: false,
-      },
-    }));
+    expect(next).toBeCalledWith(action);
+    expect(dispatch).toBeCalledWith(
+      dataFetchActions.fetch({
+        name: 'sentPosts',
+        args: {
+          profileId: action.profileId,
+          isFetchingMore: false,
+        },
+      })
+    );
   });
 });
