5e8675afa6853dfe9f5b2d84be29f8f626c986ea
Char Mays <charmaramays@gmail.com>
2019-12-05 14:34:07 -0800

    Remove app switcher modal

--- packages/app-switcher/reducer.js
+++ packages/app-switcher/reducer.js
@@ -25,7 +25,7 @@ export default (state = initialState, action) => {
       return {
         ...state,
         // User is not in phased rollout - so they can switch
-        showGoBackToClassic: !action.result.hasNewPublish,
+        showGoBackToClassic: false,
         user: {
           ...action.result,
           loading: false,
