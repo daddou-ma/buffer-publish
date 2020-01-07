535677fa874b99ad2eb658a0b5b169d6099e9031
Ana Almeida <anafilipadealmeida@gmail.com>
2020-01-07 12:00:33 +0000

    Refactor queue banner

--- packages/queue/components/RemindersBanner/index.jsx
+++ packages/queue/components/RemindersBanner/index.jsx
@@ -2,9 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Link } from '@bufferapp/components';
-import { QueueBanner } from '@bufferapp/publish-shared-components';
-import { Text } from '@bufferapp/ui';
-import { blue } from '@bufferapp/ui/style/colors';
+import { Text, Button } from '@bufferapp/ui';
+import { blue, gray } from '@bufferapp/ui/style/colors';
+
+const QueueBannerCard = styled.div`
+  display: flex;
+  border: 1px solid ${gray};
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-bottom: 24px;
+  position: relative;
+`;
+
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 24px 32px;
+  align-items: end;
+`;
+
+const Title = styled(Text)`
+  margin: 0 0 8px;
+`;
+
+const ButtonWithStyles = styled(Button)`
+  margin-top: 16px;
+`;
 
 const TextWithStyles = styled(Text)`
   margin: 0;
@@ -14,29 +37,34 @@ const TextLink = styled.span`
   color: ${blue};
 `;
 
-const content = (
-  <TextWithStyles type="p">
-    Due to Instagram limitations, we cannot auto publish some of your queued
-    posts. <br />
-    Set up mobile Reminders to make sure all your content gets publish on
-    time.&nbsp;
-    <Link
-      href="https://faq.buffer.com/article/950-publish-how-instagram-works"
-      newTab
-      unstyled
-    >
-      <TextLink>Learn more here.</TextLink>
-    </Link>
-  </TextWithStyles>
-);
-
 const RemindersBanner = ({ onSetRemindersClick }) => (
-  <QueueBanner
-    title="Uh-oh! Some of your content can't be published."
-    content={content}
-    onClick={() => onSetRemindersClick({ type: 'banner' })}
-    actionLabel="Set Up Reminders"
-  />
+  <QueueBannerCard>
+    <ContentWrapper>
+      <Title type="h3">Set up Reminders to complete your Instagram post.</Title>
+      <TextWithStyles type="p">
+        Due to Instagram limitations, we can&apos;t auto publish some of your
+        queued posts. Set up Reminders to make sure all your content gets
+        published on time.&nbsp;
+        <Link
+          href="https://faq.buffer.com/article/950-publish-how-instagram-works"
+          newTab
+          unstyled
+        >
+          <TextLink>Learn more here.</TextLink>
+        </Link>
+      </TextWithStyles>
+      <ButtonWithStyles
+        type="primary"
+        onClick={() => onSetRemindersClick({ type: 'banner' })}
+        label="Set Up Reminders"
+      />
+    </ContentWrapper>
+    <img
+      height="100%"
+      alt="push_notifications_snippet"
+      src="https://buffer-publish.s3.amazonaws.com/images/ig-reminders-snippet.png"
+    />
+  </QueueBannerCard>
 );
 
 RemindersBanner.propTypes = {
