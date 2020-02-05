2b0cea3308166ba66dce606ee9950c04348a9d45
Ana Almeida <anafilipadealmeida@gmail.com>
2020-02-05 23:11:04 +0000

    Update CampaignsPage

--- packages/campaigns/components/CampaignsPage/index.jsx
+++ packages/campaigns/components/CampaignsPage/index.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import EmptyState from '../EmptyState';
+import CreateCampaign from '../CreateCampaign';
 
-const CampaignsPage = ({ translations, onCreateCampaignClick }) => (
-  <EmptyState
-    translations={translations.emptyState}
-    onCreateCampaignClick={onCreateCampaignClick}
-  />
-);
+/* List of views available to be rendered */
+const CREATE_CAMPAIGN = 'createCampaign';
+const VIEW_CAMPAIGNS = 'viewCampaigns';
+
+/* Component */
+const CampaignsPage = ({ translations, onCreateCampaignClick }) => {
+  const [viewMode, setViewMode] = useState(VIEW_CAMPAIGNS);
+  return (
+    <React.Fragment>
+      {viewMode === VIEW_CAMPAIGNS && (
+        <EmptyState
+          translations={translations.emptyState}
+          onCreateCampaignClick={onCreateCampaignClick}
+        />
+      )}
+      {viewMode === CREATE_CAMPAIGN && (
+        <CreateCampaign
+          translations={translations.createCampaign}
+          onCancelClick={() => setViewMode(VIEW_CAMPAIGNS)}
+        />
+      )}
+    </React.Fragment>
+  );
+};
 
 CampaignsPage.propTypes = {
   translations: PropTypes.object.isRequired, // eslint-disable-line
