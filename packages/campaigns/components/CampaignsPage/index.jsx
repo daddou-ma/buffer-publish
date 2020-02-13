2b3ccd3a40bfd7f640138735c145e82c6f6a0fab
Maya Uribe <mariale.uribe@gmail.com>
2020-02-13 14:31:11 +0100

    Adding a campaign wrapper and fixing snapshot file.

--- packages/campaigns/components/CampaignsPage/index.jsx
+++ packages/campaigns/components/CampaignsPage/index.jsx
@@ -1,33 +1,8 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import ProfileSidebar from '@bufferapp/publish-profile-sidebar';
 import EmptyState from '../EmptyState';
+import CampaignsWrapper from '../CampaignsWrapper';
 import CreateCampaign from '../CreateCampaign';
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-grow: 1;
-`;
-
-const ProfileSidebarStyle = styled.div`
-  flex-basis: 16rem;
-  width: 16rem;
-  min-width: 16rem;
-  position: sticky;
-  bottom: 0;
-  top: 0;
-  max-height: 100vh;
-`;
-
-const ContentStyle = styled.div`
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-  margin-left: 0.5rem;
-  margin-right: 1rem;
-  min-height: 100%;
-`;
 
 /* List of views available to be rendered */
 const CREATE_CAMPAIGN = 'createCampaign';
@@ -37,26 +12,21 @@ const VIEW_CAMPAIGNS = 'viewCampaigns';
 const CampaignsPage = ({ translations, campaigns, onCreateCampaignClick }) => {
   const [viewMode, setViewMode] = useState(VIEW_CAMPAIGNS);
   return (
-    <Wrapper>
-      <ProfileSidebarStyle>
-        <ProfileSidebar />
-      </ProfileSidebarStyle>
-      <ContentStyle>
-        {viewMode === VIEW_CAMPAIGNS && campaigns.length === 0 && (
-          <EmptyState
-            translations={translations.emptyState}
-            onOpenCreateCampaignClick={() => setViewMode(CREATE_CAMPAIGN)}
-          />
-        )}
-        {viewMode === CREATE_CAMPAIGN && (
-          <CreateCampaign
-            translations={translations.createCampaign}
-            onCreateCampaignClick={onCreateCampaignClick}
-            onCancelClick={() => setViewMode(VIEW_CAMPAIGNS)}
-          />
-        )}
-      </ContentStyle>
-    </Wrapper>
+    <CampaignsWrapper>
+      {viewMode === VIEW_CAMPAIGNS && campaigns.length === 0 && (
+        <EmptyState
+          translations={translations.emptyState}
+          onOpenCreateCampaignClick={() => setViewMode(CREATE_CAMPAIGN)}
+        />
+      )}
+      {viewMode === CREATE_CAMPAIGN && (
+        <CreateCampaign
+          translations={translations.createCampaign}
+          onCreateCampaignClick={onCreateCampaignClick}
+          onCancelClick={() => setViewMode(VIEW_CAMPAIGNS)}
+        />
+      )}
+    </CampaignsWrapper>
   );
 };
 
