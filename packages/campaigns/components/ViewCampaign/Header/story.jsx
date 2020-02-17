245a8fff33329ad6b0ac00178bc1bf06ec9657a3
AL <amyleeannebennett@gmail.com>
2020-02-17 14:00:03 -0600

    Add story for view campaign header

---
+++ packages/campaigns/components/ViewCampaign/Header/story.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { withA11y } from '@storybook/addon-a11y';
+import translations from '@bufferapp/publish-i18n/translations/en-us.json';
+import campaignDetails from '../story';
+
+import Header from './index';
+
+storiesOf('Campaigns|ViewCampaignHeader', module)
+  .addDecorator(withA11y)
+  .add('Campaign view header with posts', () => (
+    <Header
+      campaignDetails={campaignDetails}
+      translations={translations.campaigns.viewCampaign}
+      onCreatePostClick={action('create post')}
+      onDeleteCampaignClick={action('delete campaign')}
+      onEditCampaignClick={action('edit campaign')}
+      hasPosts
+      isOwner
+    />
+  ))
+  .add('Campaign view header without posts', () => (
+    <Header
+      campaignDetails={campaignDetails}
+      translations={translations.campaigns.viewCampaign}
+      onCreatePostClick={action('create post')}
+      onDeleteCampaignClick={action('delete campaign')}
+      onEditCampaignClick={action('edit campaign')}
+      hasPosts={false}
+      isOwner
+    />
+  ))
+  .add('Campaign view header as team member', () => (
+    <Header
+      campaignDetails={campaignDetails}
+      translations={translations.campaigns.viewCampaign}
+      onCreatePostClick={action('create post')}
+      onDeleteCampaignClick={action('delete campaign')}
+      onEditCampaignClick={action('edit campaign')}
+      hasPosts={false}
+      isOwner={false}
+    />
+  ));
