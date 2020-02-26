79cec5e345c784e00000b592d3904bf4e4ea3610
Karel McGrail <karel@buffer.com>
2020-02-26 14:19:29 +0000

    Adding reconnection text to Disconnected Profile Modal screen

--- packages/profiles-disconnected-modal/components/ProfilesDisconnectedModal/index.jsx
+++ packages/profiles-disconnected-modal/components/ProfilesDisconnectedModal/index.jsx
@@ -4,33 +4,58 @@ import PropTypes from 'prop-types';
 import { Popover, Card } from '@bufferapp/components';
 import { Text, Button } from '@bufferapp/ui';
 import Avatar from '@bufferapp/ui/Avatar';
+import styled from 'styled-components';
+
+const ExtraMessageWithStyles = styled(Text)`
+  margin: 0;
+  font-weight: bold;
+`;
+
+const DisconnectedProfile = styled.div`
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid #e0e0e0;
+  padding: 8px;
+  border-radius: 4px;
+  margin: 8px 0;
+`;
+
+const AvatarContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const AvatarName = styled.div`
+  margin-left: 16px;
+`;
+
+const ModalContainer = styled.div`
+  max-width: 470px;
+  margin: 0 16px;
+`;
 
 const ProfilesDisconnectedModal = ({
   translations,
   disconnectedProfiles,
   reconnectProfile,
   hideModal,
+  extraMessage,
 }) => (
   <div style={{ position: 'fixed', zIndex: '3000' }}>
     <Popover onOverlayClick={hideModal}>
       <Card>
-        <div style={{ maxWidth: '470px', margin: '0 16px' }}>
+        <ModalContainer>
           <Text type="h3">{translations.headline}</Text>
           <Text type="p">{translations.body1}</Text>
           <Text type="p">{translations.body2}</Text>
+          {extraMessage && (
+            <ExtraMessageWithStyles type="p">
+              {extraMessage}
+            </ExtraMessageWithStyles>
+          )}
           {disconnectedProfiles.map(p => (
-            <div
-              key={p.id}
-              style={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                border: '1px solid #E0E0E0',
-                padding: '8px',
-                borderRadius: '4px',
-                margin: '8px 0',
-              }}
-            >
-              <div style={{ display: 'flex', alignItems: 'center' }}>
+            <DisconnectedProfile key={p.id}>
+              <AvatarContainer>
                 <Avatar
                   src={p.avatar_https}
                   fallbackUrl="https://s3.amazonaws.com/buffer-ui/Default+Avatar.png"
@@ -39,10 +64,10 @@ const ProfilesDisconnectedModal = ({
                   type="social"
                   network={p.service}
                 />
-                <div style={{ marginLeft: '16px' }}>
+                <AvatarName>
                   <Text weight="medium">{p.formatted_username}</Text>
-                </div>
-              </div>
+                </AvatarName>
+              </AvatarContainer>
               <Button
                 disabled={p.reconnecting}
                 onClick={() =>
@@ -54,9 +79,9 @@ const ProfilesDisconnectedModal = ({
                   p.reconnecting ? translations.reconnecting : translations.cta
                 }
               />
-            </div>
+            </DisconnectedProfile>
           ))}
-        </div>
+        </ModalContainer>
       </Card>
     </Popover>
   </div>
@@ -71,6 +96,11 @@ ProfilesDisconnectedModal.propTypes = {
   ).isRequired,
   reconnectProfile: PropTypes.func.isRequired,
   hideModal: PropTypes.func.isRequired,
+  extraMessage: PropTypes.string,
+};
+
+ProfilesDisconnectedModal.defaultProps = {
+  extraMessage: null,
 };
 
 export default ProfilesDisconnectedModal;
--- packages/profiles-disconnected-modal/index.js
+++ packages/profiles-disconnected-modal/index.js
@@ -4,10 +4,24 @@ import { actions } from './reducer';
 import ProfilesDisconnectedModal from './components/ProfilesDisconnectedModal';
 
 export default connect(
-  state => ({
-    translations: state.i18n.translations['profiles-disconnected-modal'],
-    ...state.profilesDisconnectedModal,
-  }),
+  state => {
+    const translations = state.i18n.translations['profiles-disconnected-modal'];
+    let extraMessage = null;
+    const disconnectedProfiles =
+      state?.profilesDisconnectedModal?.disconnectedProfiles;
+    const instagramPersonalProfiles = disconnectedProfiles.filter(
+      profile =>
+        profile?.service === 'instagram' && profile?.service_type === 'profile'
+    );
+    if (instagramPersonalProfiles.length > 0) {
+      extraMessage = translations?.extraMessage?.instagram;
+    }
+    return {
+      translations: state.i18n.translations['profiles-disconnected-modal'],
+      extraMessage,
+      ...state.profilesDisconnectedModal,
+    };
+  },
   dispatch => ({
     hideModal: () => dispatch(modalsActions.hideProfilesDisconnectedModal()),
     reconnectProfile: ({ id, service }) =>
