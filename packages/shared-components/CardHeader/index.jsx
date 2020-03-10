e44fcc553cb97f65958e3a1801921041bad32d3b
Ana Almeida <anafilipadealmeida@gmail.com>
2020-03-10 17:11:36 +0000

    Add new card header variation

--- packages/shared-components/CardHeader/index.jsx
+++ packages/shared-components/CardHeader/index.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Image } from '@bufferapp/components';
-import { grayLight, gray } from '@bufferapp/ui/style/colors';
-import { Text, Button } from '@bufferapp/ui';
+import { grayLight } from '@bufferapp/ui/style/colors';
+import { Text, Button, Avatar } from '@bufferapp/ui';
 
 const CardHeaderWrapper = styled.div`
   display: flex;
   border-bottom: 1px solid ${grayLight};
-  padding: 14px 16px;
+  padding: 12px 16px;
 `;
 
 const CardHeaderWrapperWithButton = styled(CardHeaderWrapper)`
@@ -19,45 +19,82 @@ const ContentWrapper = styled.div`
   display: flex;
   flex-grow: 1;
   align-items: center;
+  justify-content: space-between;
+`;
+
+const ProfileWrapper = styled.span`
+  display: flex;
+  align-items: center;
+`;
+
+const ProfileHandle = styled(Text)`
+  margin-left: 16px;
+`;
+
+const CreatorWrapper = styled.span`
+  display: flex;
+  align-items: center;
 `;
 
 const AvatarWrapper = styled.span`
-  display: flex;
   margin-right: 9px;
 `;
 
 const TextWithStyles = styled(Text)`
   font-size: 12px;
-  line-height: 14px;
+  line-height: 20px;
   margin: 0;
-  color: ${gray};
 `;
 
 const getCreatedText = creatorName =>
   creatorName ? `${creatorName} created this ` : 'Created ';
 
-const CardHeader = ({ creatorName, avatarUrl, createdAt, onPreviewClick }) => {
+const CardHeader = ({
+  profile,
+  creatorName,
+  avatarUrl,
+  createdAt,
+  onPreviewClick,
+}) => {
   const WrapperComponent = onPreviewClick
     ? CardHeaderWrapperWithButton
     : CardHeaderWrapper;
+  const hasProfileDetails = profile?.handle && profile?.type;
 
   return (
     <WrapperComponent>
       <ContentWrapper>
-        {creatorName && (
-          <AvatarWrapper>
-            <Image
-              src={avatarUrl}
-              border="circle"
-              height="1.25rem"
-              width="1.25rem"
+        {hasProfileDetails && (
+          <ProfileWrapper>
+            <Avatar
+              src={profile.avatarUrl}
+              fallbackUrl="https://s3.amazonaws.com/buffer-ui/Default+Avatar.png"
+              alt={profile.handle}
+              size="small"
+              type="social"
+              network={profile.type}
             />
-          </AvatarWrapper>
+            <ProfileHandle type="label" color="grayDark">
+              {profile.handle}
+            </ProfileHandle>
+          </ProfileWrapper>
         )}
-        <TextWithStyles type="p">
-          {getCreatedText(creatorName)}
-          {createdAt}
-        </TextWithStyles>
+        <CreatorWrapper>
+          {creatorName && !hasProfileDetails && (
+            <AvatarWrapper>
+              <Image
+                src={avatarUrl}
+                border="circle"
+                height="1.25rem"
+                width="1.25rem"
+              />
+            </AvatarWrapper>
+          )}
+          <TextWithStyles type="p" color="grayDark">
+            {getCreatedText(creatorName)}
+            {createdAt}
+          </TextWithStyles>
+        </CreatorWrapper>
       </ContentWrapper>
       {onPreviewClick && (
         <Button
@@ -76,11 +113,17 @@ CardHeader.propTypes = {
   avatarUrl: PropTypes.string.isRequired,
   createdAt: PropTypes.string.isRequired,
   onPreviewClick: PropTypes.func,
+  profile: PropTypes.shape({
+    avatarUrl: PropTypes.string,
+    handle: PropTypes.string,
+    type: PropTypes.string,
+  }),
 };
 
 CardHeader.defaultProps = {
   onPreviewClick: null,
   creatorName: null,
+  profile: null,
 };
 
 export default CardHeader;
