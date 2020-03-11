462d90ef4e00e0569b5d1e77a26dd10958d64759
Maya Uribe <mariale.uribe@gmail.com>
2020-03-11 17:26:15 +0100

    Mocking response from the server for list campaigns.

--- packages/server/rpc/campaigns/getCampaignsList/index.js
+++ packages/server/rpc/campaigns/getCampaignsList/index.js
@@ -1,22 +1,19 @@
+/* eslint-disable no-unused-vars */
 const { method } = require('@bufferapp/buffer-rpc');
-const mockedData = require('./index.test.js');
+const CAMPAIGNS_LIST = require('./listMock');
 
 module.exports = method(
   'getCampaignsList',
   'gets a list of campaigns, given the global organization id',
-  (
-    { globalOrganizationId },
-    { session },
-    res,
-    { PublishAPI = { get: jest.fn() }, parsers }
-  ) => {
-    PublishAPI.get.mockResolvedValueOnce(mockedData.CAMPAIGNS_LIST);
-    return PublishAPI.get({
+  ({ globalOrganizationId }, { session }, res, { PublishAPI, parsers }) =>
+    /*
+    PublishAPI.get({
       uri: `/1/campaigns.json`,
       session,
       params: { global_organization_id: globalOrganizationId },
     })
       .then(response => response.data.map(parsers.campaignParser))
       .catch(PublishAPI.errorHandler);
-  }
+    */
+    Promise.resolve(CAMPAIGNS_LIST.data.map(parsers.campaignParser))
 );
