4d8a99eedce9eb419307073eb821eaa74f7649a9
Maya Uribe <mariale.uribe@gmail.com>
2020-03-23 14:54:47 +0100

    Moving styles to different file.

--- packages/composer/composer/components/campaigns/CampaignHeader.jsx
+++ packages/composer/composer/components/campaigns/CampaignHeader.jsx
@@ -3,94 +3,22 @@ import PropTypes from 'prop-types';
 import { Text } from '@bufferapp/ui';
 import Select from '@bufferapp/ui/Select';
 import Tooltip from '@bufferapp/ui/Tooltip';
-import CheckmarkIcon from '@bufferapp/ui/Icon/Icons/Checkmark';
-import { gray, white, grayLight } from '@bufferapp/ui/style/colors';
 import ChevronDownIcon from '@bufferapp/ui/Icon/Icons/ChevronDown';
-import styled from 'styled-components';
 import ComposerActionCreators from '../../action-creators/ComposerActionCreators';
 import QuestionIcon from '../QuestionIcon';
-
-const SelectWrapper = styled.div`
-  display: flex;
-  min-width: 275px;
-  justify-content: center;
-  margin-left: auto;
-  p {
-    margin-left: 0px !important;
-  }
-  button {
-    margin-left: 10px;
-  }
-`;
-
-const Container = styled.div`
-  display: flex;
-  margin-bottom: 10px;
-  align-items: center;
-  justify-content: center;
-`;
-
-const Color = styled.div`
-  height: 8px;
-  width: 8px;
-  border-radius: 50%;
-  background-color: ${props => props.color};
-  margin-right: 10px;
-`;
-
-const Icon = styled.div`
-  display: flex;
-  align-items: center;
-  margin-left: 9px;
-`;
-
-const Separator = styled.div`
-  border-top: 1px solid ${grayLight};
-  margin: 15px -20px;
-`;
-
-const CustomButton = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  font-size: 14px;
-  line-height: 16px;
-  font-weight: 500;
-  border-radius: 4px;
-  cursor: pointer;
-  user-select: none;
-  position: relative;
-  transition-property: background-color, border-color, color;
-  transition-duration: 0.1s;
-  transition-timing-function: ease-in;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  min-width: 0;
-  flex: 0 0 auto;
-  font-family: 'Roboto', sans-serif;
-  padding-top: 0;
-  padding-bottom: 0;
-  padding-right: 16px;
-  padding-left: 16px;
-  height: 40px;
-  background-color: ${white};
-  border: 1px solid ${gray};
-  justify-content: space-between;
-`;
-
-const Checkmark = styled(CheckmarkIcon)`
-  margin-right: 8px;
-`;
-
-const LabelWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  flex-direction: row;
-  p {
-    margin-left: 0px;
-  }
-`;
+import {
+  SelectWrapper,
+  Container,
+  Color,
+  Icon,
+  Separator,
+  CustomButton,
+  Checkmark,
+  LabelWrapper,
+  IconWrapper,
+  ColorWrapper,
+  TextWrapper,
+} from './styles';
 
 const CustomLabel = ({ campaign, onClick }) => (
   <CustomButton onClick={onClick}>
@@ -111,20 +39,6 @@ CustomLabel.propTypes = {
   }).isRequired,
   onClick: PropTypes.func.isRequired,
 };
-
-const IconWrapper = styled.div`
-  display: flex;
-  min-width: 24px;
-`;
-
-const ColorWrapper = styled.div`
-  min-width: 18px;
-`;
-
-const TextWrapper = styled.div`
-  text-overflow: ellipsis;
-  overflow: hidden;
-`;
 
 const getLabel = (campaign, isSelected) => (
   <LabelWrapper title={campaign.name}>
