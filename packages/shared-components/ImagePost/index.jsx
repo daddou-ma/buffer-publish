fb151e266dcc4363a4a45cf975c152b984b34a8d
Ana Almeida <anafilipadealmeida@gmail.com>
2020-04-22 14:53:53 +0100

    Delete content from types of posts

--- packages/shared-components/ImagePost/index.jsx
+++ packages/shared-components/ImagePost/index.jsx
@@ -1,44 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import Lightbox from 'react-images';
-import { IdTag, Image } from '@bufferapp/components';
 import Post from '../Post';
-import UpdateTextContent from '../UpdateTextContent';
-
-const postContentStyle = {
-  display: 'flex',
-};
-
-const imageWrapperStyle = {
-  position: 'relative',
-  cursor: 'pointer',
-};
-
-const thumbnailWrapperStyle = {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  width: '9rem',
-  height: '9rem',
-  borderRadius: '4px',
-  border: '1px solid #B8B8B8',
-  background: '#F5F5F5',
-};
-
-const imageTagStyle = {
-  position: 'absolute',
-  bottom: '0.7rem',
-  left: '0.7rem',
-};
-
-const renderTag = tag => {
-  if (!tag) return;
-  return (
-    <span style={imageTagStyle}>
-      <IdTag>{tag}</IdTag>
-    </span>
-  );
-};
 
 const ImagePost = ({
   service_geolocation_name: locationName,
@@ -46,80 +7,18 @@ const ImagePost = ({
   subprofile_id: subprofileID,
   service_user_tags: userTags,
   ...props
-}) => {
-  const {
-    basic,
-    text,
-    links,
-    onImageClick,
-    imageSrc,
-    isLightboxOpen,
-    onImageClickPrev,
-    onImageClickNext,
-    onImageClose,
-    tag,
-  } = props;
-  const children = (
-    <div style={postContentStyle}>
-      <UpdateTextContent basic={basic} links={links} text={text} />
-      <div style={imageWrapperStyle} onClick={onImageClick}>
-        <div style={thumbnailWrapperStyle}>
-          <Image
-            src={imageSrc}
-            width="auto"
-            height="auto"
-            maxWidth="9rem"
-            maxHeight="9rem"
-            minHeight="7rem"
-            minWidth="7rem"
-            objectFit="cover"
-          />
-        </div>
-        <Lightbox
-          images={[{ src: `${imageSrc}` }]}
-          isOpen={isLightboxOpen}
-          onClickPrev={onImageClickPrev}
-          onClickNext={onImageClickNext}
-          onClose={onImageClose}
-          backdropClosesModal
-          showImageCount={false}
-        />
-        {renderTag(tag)}
-      </div>
-    </div>
-  );
-
-  return (
-    <Post
-      {...props}
-      locationName={locationName}
-      sourceUrl={sourceUrl}
-      subprofileID={subprofileID}
-      userTags={userTags}
-    >
-      {children}
-    </Post>
-  );
-};
+}) => (
+  <Post
+    {...props}
+    locationName={locationName}
+    sourceUrl={sourceUrl}
+    subprofileID={subprofileID}
+    userTags={userTags}
+  />
+);
 
 ImagePost.propTypes = {
   ...Post.commonPropTypes,
-  imageSrc: PropTypes.string.isRequired,
-  links: PropTypes.arrayOf(
-    PropTypes.shape({
-      rawString: PropTypes.string,
-      displayString: PropTypes.string,
-      expandedUrl: PropTypes.string,
-      indices: PropTypes.arrayOf(PropTypes.number),
-    })
-  ).isRequired,
-  text: PropTypes.string.isRequired,
-  tag: PropTypes.string,
-  isLightboxOpen: PropTypes.bool,
-  onImageClick: PropTypes.func,
-  onImageClickNext: PropTypes.func,
-  onImageClickPrev: PropTypes.func,
-  onImageClose: PropTypes.func,
 };
 
 ImagePost.defaultProps = ImagePost.defaultProps;
--- packages/shared-components/TextPost/index.jsx
+++ packages/shared-components/TextPost/index.jsx
@@ -1,48 +1,22 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Post from '../Post';
-import UpdateTextContent from '../UpdateTextContent';
-
-const postContentStyle = {
-  display: 'flex',
-};
 
 const TextPost = ({
   service_geolocation_name: locationName,
   source_url: sourceUrl,
   subprofile_id: subprofileID,
   ...props
-}) => {
-  const { basic, text, links } = props;
-  const children = (
-    <div style={postContentStyle}>
-      <UpdateTextContent basic={basic} links={links} text={text} />
-    </div>
-  );
-
-  return (
-    <Post
-      {...props}
-      locationName={locationName}
-      sourceUrl={sourceUrl}
-      subprofileID={subprofileID}
-    >
-      {children}
-    </Post>
-  );
-};
+}) => (
+  <Post
+    {...props}
+    locationName={locationName}
+    sourceUrl={sourceUrl}
+    subprofileID={subprofileID}
+  />
+);
 
 TextPost.propTypes = {
   ...Post.commonPropTypes,
-  links: PropTypes.arrayOf(
-    PropTypes.shape({
-      rawString: PropTypes.string,
-      displayString: PropTypes.string,
-      expandedUrl: PropTypes.string,
-      indices: PropTypes.arrayOf(PropTypes.number),
-    })
-  ).isRequired,
-  text: PropTypes.string.isRequired,
 };
 
 TextPost.defaultProps = Post.defaultProps;
