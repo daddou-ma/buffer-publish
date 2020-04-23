4cafbea72cc5d293ae7b689fcba774079a5817ca
Ana Almeida <anafilipadealmeida@gmail.com>
2020-04-23 10:09:07 +0100

    Replace Lightbox by Carousel

--- packages/shared-components/UpdateMediaContent/index.jsx
+++ packages/shared-components/UpdateMediaContent/index.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import Lightbox from 'react-images';
+import uuid from 'uuid/v4';
 import { IdTag, Image, MultipleImages } from '@bufferapp/components';
 import { grayLighter, gray } from '@bufferapp/ui/style/colors';
 import { borderRadius } from '@bufferapp/ui/style/borders';
+import { Carousel, Modal } from '@bufferapp/ui';
 
 const MediaWrapper = styled.div`
   position: relative;
@@ -28,85 +29,91 @@ const ThumbnailTag = styled.span`
   left: 0.7rem;
 `;
 
-const UpdateMediaContent = ({
-  onImageClick,
-  imageSrc,
-  imageUrls,
-  isLightboxOpen,
-  onImageClickPrev,
-  onImageClickNext,
-  onImageClose,
-  currentImage,
-  tag,
-  type,
-}) => {
+const UpdateMediaContent = ({ imageSrc, imageUrls, tag, type }) => {
+  // State
+  const [carouselOpen, openCarousel] = useState(false);
+
   const isMultipleImage = type === 'multipleImage';
   const isSingleImage = type === 'image' || type === 'video';
   const imageArray = isMultipleImage ? imageUrls : [imageSrc];
-  const images = imageArray?.map(url => ({ src: `${url}` }));
+
   return (
-    <MediaWrapper onClick={onImageClick}>
-      {isMultipleImage && (
-        <MultipleImages
-          border="rounded"
-          height="9rem"
-          urls={imageUrls}
-          width="9rem"
-        />
-      )}
-      {isSingleImage && (
-        <ImageWrapper>
-          <Image
-            src={imageSrc}
-            width="auto"
-            height="auto"
-            maxWidth="9rem"
-            maxHeight="9rem"
-            minHeight="7rem"
-            minWidth="7rem"
-            objectFit="cover"
+    <React.Fragment>
+      <MediaWrapper
+        onClick={() => {
+          openCarousel(true);
+        }}
+      >
+        {isMultipleImage && (
+          <MultipleImages
+            border="rounded"
+            height="9rem"
+            urls={imageUrls}
+            width="9rem"
           />
-        </ImageWrapper>
+        )}
+        {isSingleImage && (
+          <ImageWrapper>
+            <Image
+              src={imageSrc}
+              width="auto"
+              height="auto"
+              maxWidth="9rem"
+              maxHeight="9rem"
+              minHeight="7rem"
+              minWidth="7rem"
+              objectFit="cover"
+            />
+          </ImageWrapper>
+        )}
+        {isSingleImage && tag && (
+          <ThumbnailTag>
+            <IdTag>{tag}</IdTag>
+          </ThumbnailTag>
+        )}
+      </MediaWrapper>
+
+      {/* Image Carousel */}
+      {carouselOpen && (
+        <Modal
+          closeButton={{
+            callback: () => {
+              openCarousel(false);
+            },
+          }}
+          noBackground
+          dismissible
+        >
+          <div>
+            <Carousel width="400px">
+              {imageArray.map((url, index) => (
+                <img
+                  src={url}
+                  alt={`slide ${index}`}
+                  width="400"
+                  key={uuid()}
+                />
+              ))}
+            </Carousel>
+          </div>
+        </Modal>
       )}
-      <Lightbox
-        images={images}
-        isOpen={isLightboxOpen}
-        onClickPrev={onImageClickPrev}
-        onClickNext={onImageClickNext}
-        onClose={onImageClose}
-        currentImage={isMultipleImage ? currentImage : 0}
-        backdropClosesModal
-        showImageCount={false}
-      />
-      {isSingleImage && tag && (
-        <ThumbnailTag>
-          <IdTag>{tag}</IdTag>
-        </ThumbnailTag>
-      )}
-    </MediaWrapper>
+    </React.Fragment>
   );
 };
 
 UpdateMediaContent.propTypes = {
   type: PropTypes.oneOf(['text', 'image', 'multipleImage', 'link', 'video'])
     .isRequired,
-  onImageClick: PropTypes.func.isRequired,
-  onImageClickPrev: PropTypes.func.isRequired,
-  onImageClickNext: PropTypes.func.isRequired,
-  onImageClose: PropTypes.func.isRequired,
   imageSrc: PropTypes.string,
   tag: PropTypes.string,
-  isLightboxOpen: PropTypes.bool,
   imageUrls: PropTypes.arrayOf(PropTypes.string),
-  currentImage: PropTypes.number,
 };
 
 UpdateMediaContent.defaultProps = {
   imageSrc: '',
   tag: '',
-  isLightboxOpen: false,
   imageUrls: [],
-  currentImage: undefined,
 };
 
 export default UpdateMediaContent;
