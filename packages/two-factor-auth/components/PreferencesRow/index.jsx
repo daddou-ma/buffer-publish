459e9a71fa5eb0583650fdf18e410f028eaadb39
Maya Uribe <mariale.uribe@gmail.com>
2020-04-24 18:59:01 +0200

    Move preferences to i18n

--- packages/two-factor-auth/components/PreferencesRow/index.jsx
+++ packages/two-factor-auth/components/PreferencesRow/index.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { Link } from '@bufferapp/components';
 import { Text } from '@bufferapp/ui';
 import { SensitiveData } from '@bufferapp/publish-shared-components';
+import { useTranslation } from 'react-i18next';
 
 const formattedMethod = {
   '': 'Disabled',
@@ -16,56 +17,57 @@ const TwoFactorPreferencesRow = ({
   transition,
   method,
   phoneNumber,
-}) => (
-  <Fragment>
-    <Text type="p">
-      Two factor authentication adds an extra layer of security for your Buffer
-      account. Whenever you log in to your account, after entering your username
-      and password, you will be asked for a second authentication code that was
-      sent to your mobile phone via text or a free mobile app.{' '}
-      <Link
-        newTab
-        href="https://support.buffer.com/hc/en-us/articles/360038349434-Enabling-two-factor-authentication"
-      >
-        Learn more
-      </Link>
-    </Text>
-    {method && (
-      <Fragment>
-        <div style={{ margin: '16px 0 0 0' }}>
-          <Text type="p">
-            Method: <b>{formattedMethod[method]}</b>{' '}
-            <Link href="#" onClick={() => transition('CHANGE_METHOD')}>
-              Edit
-            </Link>
-          </Text>
-        </div>
-        {method === 'sms' && (
-          <div style={{ margin: '8px 0 0 0' }}>
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <Fragment>
+      <Text type="p">
+        {t('preferences.security.tfaExplanation')}
+        <Link
+          newTab
+          href="https://support.buffer.com/hc/en-us/articles/360038349434-Enabling-two-factor-authentication"
+        >
+          Learn more
+        </Link>
+      </Text>
+      {method && (
+        <Fragment>
+          <div style={{ margin: '16px 0 0 0' }}>
             <Text type="p">
-              Phone number:{' '}
-              <SensitiveData>
-                <b>{phoneNumber}</b>
-              </SensitiveData>{' '}
-              <Link href="#" onClick={() => transition('CHANGE_SMS')}>
+              Method: <b>{formattedMethod[method]}</b>{' '}
+              <Link href="#" onClick={() => transition('CHANGE_METHOD')}>
                 Edit
               </Link>
             </Text>
           </div>
-        )}
-        <div style={{ margin: '8px 0 0 0' }}>
-          <Text type="p">
-            Recovery code:{' '}
-            <Link href="#" onClick={() => transition('SHOW_RECOVERY')}>
-              View
-            </Link>
-          </Text>
-        </div>
-      </Fragment>
-    )}
-  </Fragment>
-);
-
+          {method === 'sms' && (
+            <div style={{ margin: '8px 0 0 0' }}>
+              <Text type="p">
+                Phone number:{' '}
+                <SensitiveData>
+                  <b>{phoneNumber}</b>
+                </SensitiveData>{' '}
+                <Link href="#" onClick={() => transition('CHANGE_SMS')}>
+                  Edit
+                </Link>
+              </Text>
+            </div>
+          )}
+          <div style={{ margin: '8px 0 0 0' }}>
+            <Text type="p">
+              Recovery code:{' '}
+              <Link href="#" onClick={() => transition('SHOW_RECOVERY')}>
+                View
+              </Link>
+            </Text>
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+}
+ 
 TwoFactorPreferencesRow.propTypes = {
   // machineState: PropTypes.string.isRequired,
   transition: PropTypes.func.isRequired,
