cbdb369120118d84e528c5b964124bfc99fe354e
Karel McGrail <karel@bufferapp.com>
2020-04-28 10:48:32 +0100

    Removing unnecessary comments

--- packages/composer/composer/components/shared/Button.jsx
+++ packages/composer/composer/components/shared/Button.jsx
@@ -1,16 +1,15 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
-  color: inherit; /* 1 */
-  font: inherit; /* 2 */
-  margin: 0; /* 3 */
+  color: inherit;
+  font: inherit;
+  margin: 0;
   overflow: visible;
 
   text-transform: none;
-  -webkit-appearance: button; /* 2 */
+  -webkit-appearance: button;
   cursor: pointer;
 
-  /* 3 */
   .button[disabled],
   &[data-disabled='true'] .button {
     cursor: default;
