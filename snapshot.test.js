9b40a898ac5435f8538aa160256260c94009e36e
María A Uribe <mariale.uribe@gmail.com>
2020-05-21 17:48:32 +0200

    Update storybook addon import.

--- snapshot.test.js
+++ snapshot.test.js
@@ -1,8 +1,9 @@
+import initStoryshots from '@storybook/addon-storyshots';
+
 const babelConfig = require('./babel.config.js');
 require('@babel/register')(babelConfig);
 require('core-js/stable');
 require('regenerator-runtime/runtime');
-const initStoryshots = require('@storybook/addon-storyshots').default;
 
 initStoryshots({
   suit: 'Snapshots',
