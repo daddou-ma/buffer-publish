6cf20917e97eabe4ccf259fe7b89a9a1772489d8
María A Uribe <mariale.uribe@gmail.com>
2020-05-28 11:26:54 +0200

    Add tests to the main queue and fake data builder.

--- __mocks__/@bufferapp/micro-rpc-client.js
+++ __mocks__/@bufferapp/micro-rpc-client.js
@@ -1,16 +1,21 @@
 const RPCClient = jest.genMockFromModule('@bufferapp/micro-rpc-client');
 RPCClient.fakeResult = { fake: 'yes' };
 RPCClient.fakeError = 'there was an error';
-RPCClient.prototype.call = jest.fn((name) => {
+
+RPCClient.prototype.call = jest.fn(name => {
   if (name === 'create') {
     return Promise.resolve({
       token: 'someSessionToken',
     });
-  } else if (name === 'destroy') {
+  }
+  if (name === 'destroy') {
     return Promise.resolve();
-  } else if (name === 'fail') {
+  }
+  if (name === 'fail') {
     return Promise.reject(new Error(RPCClient.fakeError));
   }
+
   return Promise.resolve(RPCClient.fakeResult);
 });
+
 module.exports = RPCClient;
---
+++ packages/queue/index.test.js
@@ -0,0 +1,194 @@
+import React from 'react';
+import {
+  render,
+  screen,
+  waitFor,
+} from '@bufferapp/publish-test-utils/utils/custom-render';
+import userEvent from '@testing-library/user-event';
+import {
+  buildUser,
+  buildProfile,
+  buildPost,
+  buildPostWithImage,
+  buildCampaign,
+} from '@bufferapp/publish-test-utils/generate-data';
+import TestBackend from 'react-dnd-test-backend';
+import { DragDropContext } from 'react-dnd';
+import RPCClient from '@bufferapp/micro-rpc-client';
+import '@bufferapp/publish-web/components/i18n';
+
+import QueuedPosts from './index';
+
+const profile = buildProfile();
+const initialState = {
+  queue: {
+    byProfileId: { [profile.id]: { posts: [], page: 0, loading: false } },
+    editMode: false,
+    showComposer: false,
+    emptySlotMode: false,
+    editingPostId: '',
+    isInstagramLoading: false,
+  },
+  profileSidebar: {
+    selectedProfileId: profile.id,
+    selectedProfile: profile,
+    profiles: [profile],
+  },
+  appSidebar: {
+    user: buildUser(),
+  },
+  drafts: {
+    draftMode: false,
+  },
+  campaignsList: {
+    campaigns: null,
+  },
+  publishProfiles: [profile],
+  user: { isOnBusinessTrial: false },
+};
+
+// eslint-disable-next-line react/prop-types
+const _TestContextContainer = ({ children }) => <>{children}</>;
+
+const queueComponents = () => {
+  const whatToShareInput = screen.getByRole('button', {
+    name: /what would you like to share?/i,
+  });
+  const dayButton = screen.getByRole('button', {
+    name: /day/i,
+  });
+  const weekButton = screen.getByRole('button', {
+    name: /week/i,
+  });
+  const monthButton = screen.getByRole('button', {
+    name: /month/i,
+  });
+  const today = screen.getByText(/today/i);
+  const date = screen.getByText(/january 1\b/i);
+  const slots = screen.getAllByText(/11:00 am/i);
+
+  return {
+    whatToShareInput,
+    dayButton,
+    weekButton,
+    monthButton,
+    today,
+    date,
+    slots,
+  };
+};
+
+const getTime = date => {
+  const day = date || new Date();
+  return Math.floor(day / 1000);
+};
+
+describe('QueuedPosts | user interaction', () => {
+  const rpcCall = RPCClient.prototype.call;
+  const TestDragDropContainer = DragDropContext(TestBackend)(
+    _TestContextContainer
+  );
+
+  const campaign = buildCampaign();
+  const day1 = new Date('2020-01-01T11:00:00.000Z');
+  const post1 = buildPost({
+    overrides: {
+      profileId: profile.id,
+      scheduledAt: getTime(day1),
+      campaignDetails: campaign,
+    },
+  });
+
+  const day2 = new Date('2020-01-02T11:00:00.000Z');
+  const post2 = buildPostWithImage({
+    overrides: {
+      profileId: profile.id,
+      scheduledAt: getTime(day2),
+    },
+  });
+
+  const posts = { [post1.id]: post1, [post2.id]: post2 };
+
+  beforeAll(() => {
+    Date.now = jest.fn(() => new Date('2020-01-01T10:10:00.000Z'));
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('should render empty queue with slots', () => {
+    render(
+      <TestDragDropContainer>
+        <QueuedPosts profileId={profile.id} />
+      </TestDragDropContainer>,
+      {
+        initialState,
+      }
+    );
+
+    const {
+      whatToShareInput,
+      dayButton,
+      weekButton,
+      monthButton,
+      today,
+      date,
+      slots,
+    } = queueComponents();
+
+    expect(whatToShareInput).toBeInTheDocument();
+    expect(dayButton).toBeInTheDocument();
+    expect(weekButton).toBeInTheDocument();
+    expect(monthButton).toBeInTheDocument();
+    expect(today).toBeInTheDocument();
+    expect(date).toBeInTheDocument();
+    expect(slots.length).toBeGreaterThan(0);
+  });
+
+  test('should render queue with slots and posts', async () => {
+    render(
+      <TestDragDropContainer>
+        <QueuedPosts profileId={profile.id} />
+      </TestDragDropContainer>,
+      {
+        initialState: {
+          ...initialState,
+          queue: {
+            ...initialState.queue,
+            byProfileId: {
+              [profile.id]: {
+                posts,
+                total: 2,
+                page: 2,
+                loading: false,
+              },
+            },
+          },
+        },
+      }
+    );
+
+    expect(rpcCall).toHaveBeenCalledWith('getCampaignsList', {});
+    expect(rpcCall).toHaveBeenCalledTimes(1);
+
+    expect(screen.getByText(post1.postContent.text)).toBeInTheDocument();
+    expect(screen.getByText(post1.postDetails.postAction)).toBeInTheDocument();
+    expect(screen.getByText(campaign.name)).toBeInTheDocument();
+
+    const shareNowButtons = screen.getAllByText(/share now/i);
+    expect(shareNowButtons.length).toBe(2);
+    userEvent.click(shareNowButtons[0]);
+
+    const sharing = await screen.findByText(/sharing.../i);
+    await waitFor(() => {
+      expect(sharing).toBeInTheDocument();
+    });
+
+    expect(rpcCall).toHaveBeenCalledWith('sharePostNow', {
+      profileId: profile.id,
+      updateId: post1.id,
+    });
+    expect(rpcCall).toHaveBeenCalledTimes(2);
+  });
+});
