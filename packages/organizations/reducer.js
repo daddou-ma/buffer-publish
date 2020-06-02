b50c56154a8ac6869e4d23544ecbfb5696be67ff
José M <jm.gilgado@gmail.com>
2020-06-02 12:25:24 +0200

    Update packages/organizations/reducer.js
    Co-authored-by: Karel McGrail <karel@bufferapp.com>

--- packages/organizations/reducer.js
+++ packages/organizations/reducer.js
@@ -1,8 +1,4 @@
-const orgData =
-  typeof window !== 'undefined' &&
-  typeof window.bufferData !== 'undefined' &&
-  typeof window.bufferData.organizations !== 'undefined' &&
-  window.bufferData.organizations;
+const orgData = window?.bufferData?.organizations;
 
 module.exports = (state = orgData || {}, action) => {
   switch (action.type) {
