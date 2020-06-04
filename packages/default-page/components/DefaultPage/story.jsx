bb1a037b4d8e766cc2f33f97e56048753373a527
Ana Almeida <anafilipadealmeida@gmail.com>
2020-06-04 23:45:58 +0100

    Remove onClick

--- packages/default-page/components/DefaultPage/story.jsx
+++ packages/default-page/components/DefaultPage/story.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withA11y } from '@storybook/addon-a11y';
-import { action } from '@storybook/addon-actions';
 import DefaultPage from './index';
 
 storiesOf('DefaultPage', module)
   .addDecorator(withA11y)
-  .add('admin user', () => (
-    <DefaultPage
-      onConnectSocialAccountClick={action('connect social account click')}
-    />
-  ))
+  .add('admin user', () => <DefaultPage isAdmin />)
   .add('non admin user', () => (
-    <DefaultPage isAdmin ownerEmail="ana@buffer.com" orgName="Cool Org" />
+    <DefaultPage
+      isAdmin={false}
+      ownerEmail="ana@buffer.com"
+      orgName="Cool Org"
+    />
   ));
--- packages/default-page/index.js
+++ packages/default-page/index.js
@@ -1,22 +1,14 @@
 import { connect } from 'react-redux';
-import { actions } from './reducer';
 
 import DefaultPage from './components/DefaultPage';
 
-export default connect(
-  state => ({
-    orgName: state.organizations.selected.name,
-    ownerEmail: state.organizations.selected.ownerEmail,
-    isAdmin:
-      state.user.features?.includes('org_switcher') &&
-      state.organizations.selected.isAdmin,
-  }),
-  dispatch => ({
-    onConnectSocialAccountClick: () => {
-      dispatch(actions.handleConnectSocialAccountClick());
-    },
-  })
-)(DefaultPage);
+export default connect(state => ({
+  orgName: state.organizations.selected?.name,
+  ownerEmail: state.organizations.selected?.ownerEmail,
+  isAdmin:
+    state.user.features?.includes('org_switcher') &&
+    state.organizations.selected?.isAdmin,
+}))(DefaultPage);
 
 export { actions, actionTypes } from './reducer';
 export middleware from './middleware';
