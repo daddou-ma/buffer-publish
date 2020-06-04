25004277ba058f120dc7e7904faab2ff4ea00bfa
Ana Almeida <anafilipadealmeida@gmail.com>
2020-06-04 23:48:33 +0100

    Delete default-page middleware and reducer files

--- packages/store/middlewares.js
+++ packages/store/middlewares.js
@@ -32,7 +32,6 @@ import manageAppsMiddleware from '@bufferapp/manage-apps-extras/middleware';
 import twoFactorAuthMiddleware from '@bufferapp/publish-two-factor-auth/middleware';
 import dateTimePreferencesMiddleware from '@bufferapp/date-time-preferences/middleware';
 import maintenanceRedirectMiddleware from '@bufferapp/maintenance-redirect/middleware';
-import defaultPageMiddleware from '@bufferapp/default-page/middleware';
 import instagramDirectPostingModalMiddleware from '@bufferapp/publish-ig-direct-posting-modal/middleware';
 import notificationsProviderMiddleware from '@bufferapp/publish-notifications-provider/middleware';
 import profilesDisconnectedModalMiddleware from '@bufferapp/publish-profiles-disconnected-modal/middleware';
@@ -97,7 +96,6 @@ const composedMiddlewares = history =>
       modalsMiddleware,
       twoFactorAuthMiddleware,
       dateTimePreferencesMiddleware,
-      defaultPageMiddleware,
       instagramDirectPostingModalMiddleware,
       maintenanceRedirectMiddleware,
       draftsMiddleware,
