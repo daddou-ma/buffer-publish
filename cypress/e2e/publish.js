efd8720642d1814836beff37b8d5daaad2e74f59
María A Uribe <mariale.uribe@gmail.com>
2020-06-18 18:44:44 +0200

    Add an await for auth in e2e test before clicking in the composer button.

--- cypress/e2e/publish.js
+++ cypress/e2e/publish.js
@@ -6,13 +6,21 @@ const getRandomPostText = () => {
 
 describe('Publish', () => {
   it('creates a new post', () => {
+    cy.server();
+    cy.route({
+      method: 'POST',
+      url: '/pusher/auth',
+      status: 200,
+    }).as('auth');
+
     cy.login();
     cy.visit('/');
+    cy.wait('@auth');
     cy.get('[data-cy=open-composer-button]').click();
+
     const randomPostText = getRandomPostText();
-    cy.get('[data-cy=composer-text-zone]').type(
-      `${randomPostText}{cmd}{enter}`
-    );
-    cy.get('[data-cy=post]').should('contain', randomPostText);
+    cy.get('[data-cy=composer-text-zone]').type(`${randomPostText}`);
+    cy.findByText(/add to queue/i).click();
+    cy.findByText(randomPostText).should('exist');
   });
 });
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,3 +1,5 @@
+import '@testing-library/cypress/add-commands';
+
 /**
  * Import our custom Cypress commands
  */
