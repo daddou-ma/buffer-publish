57053d59d79945c8ff99289588761ee0a44acc83
María A Uribe <mariale.uribe@gmail.com>
2020-06-22 12:57:56 +0200

    Extract share again and send to mobile buttons to shared-components.

--- packages/shared-components/PostLists/index.jsx
+++ packages/shared-components/PostLists/index.jsx
@@ -2,19 +2,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Button } from '@bufferapp/ui';
 import { WithFeatureLoader } from '@bufferapp/product-features';
 
 import Post from '../Post';
 import Story from '../Story';
 import QueueHeader from '../QueueHeader';
+import PostActionButtons from '../PostActionButtons';
 import BannerAdvancedAnalytics from '../BannerAdvancedAnalytics';
-
-const ShareAgainWrapper = styled.div`
-  padding-left: 1rem;
-  padding-bottom: 0.5rem;
-  min-width: 146px;
-`;
 
 const PostStyle = styled.div`
   display: flex;
@@ -23,7 +17,7 @@ const PostStyle = styled.div`
     props.shouldShowAnalyzeBanner ? '2rem' : '2.5rem'};
 `;
 
-const renderPost = ({
+const RenderPost = ({
   item,
   index,
   onDeleteConfirmClick,
@@ -60,6 +54,9 @@ const renderPost = ({
 
   const shouldShowAnalyzeBanner =
     showAnalyzeBannerAfterFirstPost && index === 1;
+  const shouldShowShareAgainButton =
+    !features.isFreeUser() || isBusinessAccount;
+  const shouldShowSendToMobileButton = isPastReminder && isManager;
 
   const PostComponent = item.type === 'storyGroup' ? Story : Post;
 
@@ -78,45 +75,16 @@ const renderPost = ({
         shouldShowAnalyzeBanner
       >
         <PostComponent {...postWithEventHandlers} />
-        {(!features.isFreeUser() || isBusinessAccount) && !isPastReminder && (
-          <ShareAgainWrapper>
-            <Button
-              type="secondary"
-              label="Share Again"
-              onClick={() => {
-                onShareAgainClick({ post: item });
-              }}
-            />
-          </ShareAgainWrapper>
-        )}
-        {isPastReminder && (
-          <div>
-            {(!features.isFreeUser() || isBusinessAccount) && (
-              <ShareAgainWrapper>
-                <Button
-                  fullWidth
-                  type="secondary"
-                  label="Share Again"
-                  onClick={() => {
-                    onShareAgainClick({ post: item });
-                  }}
-                />
-              </ShareAgainWrapper>
-            )}
-            {isManager && (
-              <ShareAgainWrapper>
-                <Button
-                  fullWidth
-                  type="secondary"
-                  label="Send to Mobile"
-                  onClick={() => {
-                    onMobileClick({ post: item });
-                  }}
-                />
-              </ShareAgainWrapper>
-            )}
-          </div>
-        )}
+        <PostActionButtons
+          shouldShowShareAgainButton={shouldShowShareAgainButton}
+          shouldShowSendToMobileButton={shouldShowSendToMobileButton}
+          onShareAgainClick={() => {
+            onShareAgainClick({ post: item });
+          }}
+          onMobileClick={() => {
+            onMobileClick({ post: item });
+          }}
+        />
       </PostStyle>
       {shouldShowAnalyzeBanner && (
         <BannerAdvancedAnalytics isAnalyzeCustomer={isAnalyzeCustomer} />
@@ -142,7 +110,7 @@ const PostLists = ({ items, ...propsForPosts }) => {
       );
     }
     if (queueItemType === 'post') {
-      return renderPost({ index, item: rest, ...propsForPosts });
+      return <RenderPost index={index} item={rest} {...propsForPosts} />;
     }
 
     return null;
@@ -162,14 +130,9 @@ PostLists.propTypes = {
       hasCommentEnabled: PropTypes.bool,
     })
   ).isRequired,
-  isBusinessAccount: PropTypes.bool,
   features: PropTypes.shape({
     isFreeUser: () => {},
   }).isRequired,
-};
-
-PostLists.defaultProps = {
-  isBusinessAccount: false,
 };
 
 export default WithFeatureLoader(PostLists);
