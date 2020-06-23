2e64b7ad544e4d437fe025ba67532ac676c11188
Ana Almeida <anafilipadealmeida@gmail.com>
2020-06-23 12:23:19 +0100

    Update rpc method

--- packages/server/rpc/setCurrentOrganization/index.js
+++ packages/server/rpc/setCurrentOrganization/index.js
@@ -1,16 +1,29 @@
-const { method } = require('@bufferapp/buffer-rpc');
+const { method, createError } = require('@bufferapp/buffer-rpc');
+const rp = require('request-promise');
 
 module.exports = method(
   'setCurrentOrganization',
   'set current organization',
-  ({ organizationId }, { session }, res, { PublishAPI }) =>
-    PublishAPI.post({
-      uri: '1/user/set_selected_organization.json',
-      session,
-      params: {
+  ({ organizationId }, { session }) =>
+    rp({
+      uri: `${process.env.API_ADDR}/1/user/set_selected_organization.json`,
+      method: 'POST',
+      strictSSL: !(
+        process.env.NODE_ENV === 'development' ||
+        process.env.NODE_ENV === 'test'
+      ),
+      qs: {
+        access_token: session.publish.accessToken,
         organization_id: organizationId,
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
       },
     })
-      .then(response => JSON.parse(response.data))
-      .catch(PublishAPI.errorHandler)
+      .then(result => JSON.parse(result))
+      .catch(err => {
+        if (err.error) {
+          const error = JSON.parse(err.error);
+          throw createError({ message: error.message });
+        }
+      })
 );
