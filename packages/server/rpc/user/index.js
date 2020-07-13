11f636dfaa433f1a12e56e239e982e2c826276d3
Ana Almeida <anafilipadealmeida@gmail.com>
2020-07-13 14:30:42 +0100

    Inject orgs data in user

--- packages/server/rpc/user/index.js
+++ packages/server/rpc/user/index.js
@@ -3,14 +3,37 @@ const { method } = require('@bufferapp/buffer-rpc');
 module.exports = method(
   'user',
   'fetch user data',
-  (_, { session }, res, { PublishAPI, parsers }) =>
-    PublishAPI.get({
+  async (_, { session }, res, { PublishAPI, parsers }) => {
+    const userData = await PublishAPI.get({
       uri: `1/user.json`,
       session,
       params: {
         includes: 'avatar',
       },
-    })
-      .then(user => parsers.userParser(user))
-      .catch(PublishAPI.errorHandler)
+    }).then(user => parsers.userParser(user));
+    const hasOrgSwitcher =
+      userData &&
+      userData.features &&
+      userData.features.includes('org_switcher');
+
+    // Temporarily injecting org plan data in users. To be removed after org switcher rollout.
+    if (hasOrgSwitcher) {
+      const orgs = await PublishAPI.get({
+        uri: `1/user/organizations.json`,
+        session,
+      });
+
+      const orgSelected =
+        (orgs && orgs.data && orgs.data.find(org => org.selected)) || orgs[0];
+      const { plan_code } = orgSelected;
+
+      return {
+        ...userData,
+        planCode: plan_code,
+      };
+    }
+    return {
+      ...userData,
+    };
+  }
 );
