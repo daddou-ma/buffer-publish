2ad531ec63ae9a74ad73d1cec7720e967e83dbb2
Ana Almeida <anafilipadealmeida@gmail.com>
2020-07-14 15:06:25 +0100

    Sort orgs alphabetical order

--- packages/organizations/utils/index.js
+++ packages/organizations/utils/index.js
@@ -2,6 +2,9 @@ export const getSelectedOrganization = organizations => {
   return organizations?.find(org => org.selected) || organizations[0];
 };
 
+export const getOrgsAlfabeticalOrder = organizations =>
+  Object.values(organizations)?.sort((a, b) => (a.name > b.name ? 1 : -1));
+
 export const mapSelectedOrganization = ({ id, organizations }) => {
   return organizations.map(org => ({
     ...org,
