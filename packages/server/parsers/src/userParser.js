c4ecf51806b3e2e36a1005a8e7dd26908e3e818b
Ana Almeida <anafilipadealmeida@gmail.com>
2020-07-16 11:49:37 +0100

    Use billing_plan instead of deprecated plan

--- packages/server/parsers/src/userParser.js
+++ packages/server/parsers/src/userParser.js
@@ -11,11 +11,11 @@ module.exports = userData => ({
   tags: userData.tags,
   hasTwentyFourHourTimeFormat: userData.twentyfour_hour_time,
   imageDimensionsKey: userData.imagedimensions_key,
-  plan: userData.plan,
+  plan: userData.billing_plan_tier,
   planCode: userData.plan_code,
-  isBusinessUser: userData.plan_code >= 8 && userData.plan_code <= 19,
-  isFreeUser: userData.plan_code === 1,
-  isProUser: userData.plan_code === 5 || userData.plan_code === 6,
+  isBusinessUser: userData.billing_plan_base === 'business',
+  isFreeUser: userData.billing_plan_base === 'free',
+  isProUser: userData.billing_plan_base === 'pro',
   messages: userData.messages || [],
   skip_empty_text_alert: userData.messages.includes(
     'remember_confirm_saving_modal'
@@ -52,13 +52,12 @@ module.exports = userData => ({
   canStartProTrial: userData.can_start_pro_trial,
   shouldShowProTrialExpiredModal:
     hasProTrialExpired(userData.feature_trials) &&
-    userData.plan === 'free' &&
+    userData.billing_plan_base === 'free' &&
     !userData.has_cancelled,
   isOnBusinessTrial:
-    userData.plan_code >= 8 && userData.plan_code <= 19 && userData.on_trial,
+    userData.billing_plan_base === 'business' && userData.on_trial,
   shouldShowBusinessTrialExpiredModal:
-    userData.plan_code >= 8 &&
-    userData.plan_code <= 19 &&
+    userData.billing_plan_base === 'business' &&
     userData.on_trial &&
     userData.trial_expired &&
     !userData.trial_done,
