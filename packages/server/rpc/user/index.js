f38fcac56183061d01df3da280e89cae33d99279
Ana Almeida <anafilipadealmeida@gmail.com>
2020-07-20 13:24:06 +0100

    Change rpc method

--- packages/server/rpc/user/index.js
+++ packages/server/rpc/user/index.js
@@ -4,36 +4,38 @@ module.exports = method(
   'user',
   'fetch user data',
   async (_, { session }, res, { PublishAPI, parsers }) => {
-    const userData = await PublishAPI.get({
-      uri: `1/user.json`,
-      session,
-      params: {
-        includes: 'avatar',
-      },
-    }).then(user => parsers.userParser(user));
-    const hasOrgSwitcher =
-      userData &&
-      userData.features &&
-      userData.features.includes('org_switcher');
-
-    // Temporarily injecting org plan data in users. To be removed after org switcher rollout.
-    if (hasOrgSwitcher) {
-      const orgs = await PublishAPI.get({
+    return Promise.all([
+      PublishAPI.get({
+        uri: `1/user.json`,
+        session,
+        params: {
+          includes: 'avatar',
+        },
+      }).catch(PublishAPI.errorHandler),
+      PublishAPI.get({
         uri: `1/user/organizations.json`,
         session,
+      }),
+    ])
+      .then(([user, { data }]) => {
+        return [
+          parsers.userParser(user),
+          data.map(org => parsers.orgParser(org)),
+        ];
+      })
+      .then(([user, orgs]) => {
+        const hasOrgSwitcher =
+          user && user.features && user.features.includes('org_switcher');
+        const orgSelected =
+          (orgs && orgs.data && orgs.data.find(org => org.selected)) || orgs[0];
+        if (orgSelected) {
+          const { planCode } = orgSelected;
+          // Temporarily injecting org plan data in users. To be removed after org switcher rollout.
+          if (hasOrgSwitcher) {
+            return { ...user, planCode };
+          }
+        }
+        return user;
       });
-
-      const orgSelected =
-        (orgs && orgs.data && orgs.data.find(org => org.selected)) || orgs[0];
-      const { plan_code } = orgSelected;
-
-      return {
-        ...userData,
-        planCode: plan_code,
-      };
-    }
-    return {
-      ...userData,
-    };
   }
 );
