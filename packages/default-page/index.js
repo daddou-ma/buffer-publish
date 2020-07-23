c5b99f5fa7be9b3d44246a21a5010f79e57953ef
Ana Almeida <anafilipadealmeida@gmail.com>
2020-07-23 19:59:20 +0100

    Fix org switcher

--- packages/default-page/index.js
+++ packages/default-page/index.js
@@ -6,6 +6,5 @@ export default connect(state => ({
   orgName: state.organizations.selected?.name,
   ownerEmail: state.organizations.selected?.ownerEmail,
   showPermissionsEmptyPage:
-    state.user.features?.includes('org_switcher') &&
-    !state.organizations.selected?.isAdmin,
+    state.user.hasOrgSwitcherFeature && !state.organizations.selected?.isAdmin,
 }))(DefaultPage);
