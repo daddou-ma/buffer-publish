3b40fd82b7c3ba966d026ed459978ac7325a2a9e
Federico Weber <federicoweber@gmail.com>
2020-07-23 08:04:56 -0700

    fix analytics RPCs test

--- packages/server/rpc/analytics/postsSummary/mockResponses.js
+++ packages/server/rpc/analytics/postsSummary/mockResponses.js
@@ -1,24 +1,28 @@
 export const CURRENT_PERIOD_RESPONSE = {
   response: {
-    comments: 100,
-    post_impressions: 56755,
-    post_reach: 1181030,
-    posts_count: 3,
-    reactions: 9391,
-    shares: 5,
-    engagement_rate: 12,
+    organic: {
+      comments: 100,
+      post_impressions: 56755,
+      post_reach: 1181030,
+      posts_count: 3,
+      reactions: 9391,
+      shares: 5,
+      engagement_rate: 12,
+    }
   },
   success: true,
 };
 export const PAST_PERIOD_RESPONSE = {
   response: {
-    comments: 80,
-    post_impressions: 45555,
-    post_reach: 1181090,
-    posts_count: 5,
-    reactions: 9391,
-    shares: 2,
-    engagement_rate: 6,
+    organic: {
+      comments: 80,
+      post_impressions: 45555,
+      post_reach: 1181090,
+      posts_count: 5,
+      reactions: 9391,
+      shares: 2,
+      engagement_rate: 6,
+    }
   },
   success: true,
 };
