bb2955855639fd2e6ad87b03cd73c005a7b36fe7
Hamish Macpherson <hamstu@gmail.com>
2020-09-02 09:57:59 -0700

    remove user only

--- packages/server/lib/generateIndexHtml.js
+++ packages/server/lib/generateIndexHtml.js
@@ -3,15 +3,31 @@ const { join } = require('path');
 
 const getSegmentScript = require('./embeds/segment');
 const getStripeScript = require('./embeds/stripe');
+const getNotificationScript = require('./embeds/notification');
+const getModalScript = require('./embeds/modal');
 const getAppcuesScript = require('./embeds/appcues');
+const getFullstoryScript = require('./embeds/fullstory');
 const getIterateScript = require('./embeds/iterate');
+const getBugsnagScript = require('./embeds/bugsnag');
+const getBufferDataScript = require('./embeds/bufferData');
 const getBundleReminderHtml = require('./embeds/bundleReminder');
 const getDatadogRumScript = require('./embeds/datadogRum');
 
 const { getFaviconCode } = require('./favicon');
 const { getRuntimeScript } = require('./assets');
 
-const getHtml = ({ staticAssets, isProduction, isStandalone }) => {
+const getHtml = ({
+  staticAssets,
+  isProduction,
+  isStandalone,
+  notification,
+  userId,
+  modalKey,
+  modalValue,
+  user,
+  profiles,
+  organizations,
+}) => {
   return fs
     .readFileSync(join(__dirname, '..', 'index.html'), 'utf8')
     .replace(
@@ -22,6 +38,8 @@ const getHtml = ({ staticAssets, isProduction, isStandalone }) => {
     .replace('{{{bundle}}}', staticAssets['bundle.js'])
     .replace('{{{bundle-css}}}', staticAssets['bundle.css'])
     .replace('{{{stripeScript}}}', getStripeScript())
+    .replace('{{{notificationScript}}}', getNotificationScript(notification))
+    .replace('{{{showModalScript}}}', getModalScript(modalKey, modalValue))
     .replace('{{{appcues}}}', getAppcuesScript({ isProduction, isStandalone }))
     .replace(
       '{{{iterateScript}}}',
@@ -29,6 +47,22 @@ const getHtml = ({ staticAssets, isProduction, isStandalone }) => {
     )
     .replace('{{{favicon}}}', getFaviconCode({ cacheBust: 'v1' }))
     .replace('{{{segmentScript}}}', getSegmentScript({ isProduction }))
+    .replace(
+      '{{{bufferData}}}',
+      getBufferDataScript({
+        user,
+        profiles,
+        organizations,
+      })
+    )
+    .replace(
+      '{{{bugsnagScript}}}',
+      getBugsnagScript({ userId, isProduction, isStandalone })
+    )
+    .replace(
+      '{{{fullStoryScript}}}',
+      getFullstoryScript({ user, isProduction, isStandalone })
+    )
     .replace(
       '{{{bundleReminder}}}',
       getBundleReminderHtml({ isProduction, isStandalone })
