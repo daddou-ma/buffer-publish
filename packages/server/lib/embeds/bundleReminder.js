2a68bd194bb4b56b3db6c6c579f5e05cd4909394
Hamish Macpherson <hamstu@gmail.com>
2020-09-14 14:58:22 -0700

    deleted old embeds

--- packages/server/lib/embeds/bundleReminder.js
+++
@@ -1,4 +0,0 @@
-module.exports = ({ isProduction, isStandalone }) =>
-  !isProduction || isStandalone
-    ? `<p style="text-align: center">Don't forget to bundle the JavaScript: <code>yarn run watch</code></p>`
-    : '';
