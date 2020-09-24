d94df8380fdec5e620dfcef903350cd0b0153d94
Hamish Macpherson <hamstu@gmail.com>
2020-09-24 15:47:28 -0700

    use desktop as test baseline

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,3 +1,19 @@
+/**
+ * @todo Consider adding performance budgets
+ * https://github.com/GoogleChrome/lighthouse-ci/blob/master/docs/configuration.md#budgetsjson
+ */
+
+// https://github.com/GoogleChrome/lighthouse/blob/bc6ab76a33f50403e4bfc84eb069b070ed10466f/lighthouse-core/config/constants.js#L43-L51
+// Using a "broadband" connection type
+const desktopDense4G = {
+  rttMs: 40,
+  throughputKbps: 10 * 1024,
+  cpuSlowdownMultiplier: 1,
+  requestLatencyMs: 0, // 0 means unset
+  downloadThroughputKbps: 0,
+  uploadThroughputKbps: 0,
+};
+
 module.exports = {
   ci: {
     collect: {
@@ -6,6 +22,18 @@ module.exports = {
       startServerReadyPattern: /Publish is now running/,
       settings: {
         chromeFlags: '--ignore-certificate-errors',
+        emulatedFormFactor: 'desktop',
+        throttling: desktopDense4G,
+        /** The method used to throttle the network. */
+        // throttlingMethod: 'devtools'|'simulate'|'provided';
+        /** The throttling config settings. */
+        // throttling: {} // ThrottlingSettings;
+        /** If present, the run should only conduct this list of audits. */
+        // onlyAudits: string[] | null;
+        /** If present, the run should only conduct this list of categories. */
+        // onlyCategories: string[] | null;
+        /** If present, the run should skip this list of audits. */
+        // skipAudits: string[] | null;
       },
     },
     upload: {
