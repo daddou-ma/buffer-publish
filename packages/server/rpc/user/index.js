9e466e55fec779222e3dcbd1cfac64fd762017a6
Ana Almeida <anafilipadealmeida@gmail.com>
2020-10-01 10:00:05 +0100

    Remove org injection in users

--- packages/server/rpc/user/index.js
+++ packages/server/rpc/user/index.js
@@ -3,83 +3,14 @@ const { method } = require('@bufferapp/buffer-rpc');
 module.exports = method(
   'user',
   'fetch user data',
-  async (_, { session }, res, { PublishAPI, parsers }) => {
-    return Promise.all([
-      PublishAPI.get({
-        uri: `1/user.json`,
-        session,
-        params: {
-          includes: 'avatar',
-        },
-      }),
-      PublishAPI.get({
-        uri: `1/user/organizations.json`,
-        session,
-      }).catch(() => {
-        return [];
-      }),
-    ])
-      .then(([user, { data }]) => {
-        return [
-          parsers.userParser(user),
-          data.map(org => parsers.orgParser(org)),
-        ];
-      })
-      .then(([user, orgs]) => {
-        const orgSelected = (orgs && orgs.find(org => org.selected)) || orgs[0];
-        if (orgSelected) {
-          const {
-            planCode,
-            planBase,
-            plan,
-            profilesCount,
-            usersCount,
-            ownerFeatures,
-            isOwner,
-            trial,
-          } = orgSelected;
-          let orgTrialData = {};
-          if (trial) {
-            orgTrialData = {
-              trial: {
-                hasCardDetails: trial && trial.hasCardDetails,
-                onTrial: trial.onTrial,
-                postTrialCost: trial && trial.postTrialCost,
-                trialLength: trial && trial.length,
-                trialTimeRemaining: trial && trial.timeRemaining,
-                trialPlan: trial && trial.plan,
-              },
-              canStartProTrial: trial && trial.canStartProTrial && isOwner,
-              shouldShowProTrialExpiredModal:
-                trial &&
-                trial.plan === 'pro' &&
-                trial.onTrial &&
-                trial.isExpired,
-              shouldShowBusinessTrialExpiredModal:
-                trial &&
-                trial.plan !== 'pro' &&
-                trial.onTrial &&
-                trial.isExpired &&
-                !trial.isDone,
-            };
-          }
-          // Temporarily injecting org plan data in users. To be removed after org switcher rollout.
-          return {
-            ...user,
-            plan,
-            planCode,
-            planBase,
-            features: ownerFeatures,
-            profileCount: profilesCount,
-            orgUserCount: usersCount,
-            isFreeUser: planBase === 'free',
-            analyzeCrossSale: user.analyzeCrossSale && isOwner,
-            canSeeOrgSwitcher: orgs && orgs.length >= 2,
-            ...orgTrialData,
-          };
-        }
-        return user;
-      })
-      .catch(PublishAPI.errorHandler);
-  }
+  (_, { session }, res, { PublishAPI, parsers }) =>
+    PublishAPI.get({
+      uri: `1/user.json`,
+      session,
+      params: {
+        includes: 'avatar',
+      },
+    })
+      .then(user => parsers.userParser(user))
+      .catch(PublishAPI.errorHandler)
 );
