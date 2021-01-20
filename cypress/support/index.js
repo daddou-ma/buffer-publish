e91cf55d40ada5bb169d343625dd163c5fe4f4fe
Hamish Macpherson <hamstu@gmail.com>
2021-01-20 15:05:50 -0800

    [Spike] Basic Cypress Test (#1654)

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -5,14 +5,3 @@ import '@testing-library/cypress/add-commands';
  */
 import './login';
 import './commands';
-
-/**
- * Install package that adds console messages to the Cypress output
- * to help when debugging issues.
- * https://github.com/archfz/cypress-terminal-report
- */
-require('cypress-terminal-report').installSupport();
-
-before(() => {
-  cy.createTestUser();
-});
