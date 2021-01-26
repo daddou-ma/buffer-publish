4cfd26eb114566fd552e76371bd60dcf6d706403
AL <amyleeannebennett@gmail.com>
2021-01-26 13:10:54 -0600

    Add initialState to globalAccount

--- packages/globalAccount/reducer.js
+++ packages/globalAccount/reducer.js
@@ -1,6 +1,10 @@
 import { actionTypes as asyncDataFetchActionTypes } from '@bufferapp/async-data-fetch';
 
-export default (state = {}, action) => {
+const initialState = {
+  shouldRedirectToAccountChannels: false,
+};
+
+export default (state = initialState, action) => {
   switch (action.type) {
     case `globalAccount_${asyncDataFetchActionTypes.FETCH_SUCCESS}`: {
       const featureFlips = action.result.featureFlips || [];
