6b299aa12d563777f48b2e9fb4be6bb8a2efb564
AL <amyleeannebennett@gmail.com>
2021-02-08 17:48:31 -0600

    Add global state component

---
+++ packages/shared-components/GlobalEmptyState/index.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import States from '@bufferapp/ui/States';
+import Button from '@bufferapp/ui/Button';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100%;
+`;
+
+export default function GlobalEmptyState({
+  imageSrc,
+  altText,
+  header,
+  description,
+  secondaryAction,
+  primaryAction = null,
+}) {
+  return (
+    <Wrapper>
+      <States>
+        <States.Image src={imageSrc} alt={altText} />
+        <States.Header>{header}</States.Header>
+        <States.Description>{description}</States.Description>
+        <States.Buttons>
+          <Button
+            type="secondary"
+            onClick={secondaryAction.onClick}
+            label={secondaryAction.label}
+          />
+          {primaryAction && (
+            <Button
+              type="primary"
+              onClick={primaryAction.onClick}
+              label={primaryAction.label}
+            />
+          )}
+        </States.Buttons>
+      </States>
+    </Wrapper>
+  );
+}
+
+GlobalEmptyState.propTypes = {
+  header: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  imageSrc: PropTypes.string.isRequired,
+  altText: PropTypes.string.isRequired,
+  // eslint-disable-next-line react/require-default-props
+  primaryAction: PropTypes.shape({
+    onClick: PropTypes.func,
+    label: PropTypes.string,
+  }),
+  secondaryAction: PropTypes.shape({
+    onClick: PropTypes.func,
+    label: PropTypes.string,
+  }).isRequired,
+};
