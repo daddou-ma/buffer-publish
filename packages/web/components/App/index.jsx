7d241c0e713383343f43d9909e0a6c615c127f7e
Dace <dacewillmott@gmail.com>
2021-02-19 14:45:18 -0500

    Add reconnect channel banner

--- packages/web/components/App/index.jsx
+++ packages/web/components/App/index.jsx
@@ -38,7 +38,19 @@ class App extends Component {
   render() {
     return (
       <div style={appStyle} className="notranslate">
-        <AppShell>
+        <AppShell
+          bannerOptions={{
+            text:
+              'We have had some troubles with a recent deploy; if your posts are in a failed state, please try reconnecting your channel!',
+            actionButton: {
+              label: 'Learn more',
+              action: () =>
+                window.location ===
+                'https://support.buffer.com/hc/en-us/articles/360038961173-Reconnecting-a-social-channel-in-Buffer-Publish',
+            },
+            themeColor: 'orange',
+          }}
+        >
           <div style={contentStyle}>
             <LoadingGate>
               <CTABanner />
